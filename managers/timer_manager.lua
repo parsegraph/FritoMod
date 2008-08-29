b114ff81211b9d13bc00e40006953a04d0919ae5
Aaron Faanes <dafrito@fritocomp.(none)>
2008-08-29 01:49:50 -0500

    Made TimerManager have a granularity

--- managers/timer_manager.lua
+++ managers/timer_manager.lua
@@ -7,21 +7,32 @@ function TimerManager:GetCurrentTime()
   return GetTime();
 end;
 
-function TimerManager:RegisterTimer(timerFunc)
-  self.timers[timerFunc] = GetTime();
-  if not self.lastIteration then
-    self.lastIteration = self:GetCurrentTime();
-  end;
+function TimerManager:RegisterTimer(timerFunc, granularity)
+    if not granularity then
+        granularity = 0
+    end
+    self.timers[timerFunc] = {
+        func = timerFunc,
+        granularity = granularity,
+        firstIteration = nil,
+        lastIteration = nil,
+    }
 end;
 
 function TimerManager:UnregisterTimer(timerFunc)
-  table.remove(self.timers, timerFunc);
+    table.remove(self.timers, timerFunc);
 end;
 
 function TimerManager:OnUpdate()
-  local currentTime = self:GetCurrentTime();
-  for timerFunc, startTime in ipairs(self.timers) do
-    timerFunc(currentTime - startTime, currentTime - self.lastIteration);
-  end;
-  self.lastIteration = currentTime;
+    local currentTime = self:GetCurrentTime();
+    for i, timer in ipairs(self.timers) do
+        if not timer.lastIteration then
+            timer.lastIteration = currentTime
+            timer.firstIteration = currentTime
+        end
+        if not timer.granularity or timer.lastIteration + timer.granularity >= currentTime then
+            timer.func(currentTime - timer.firstIteration, currentTime - timer.lastIteration)
+            timer.lastIteration = currentTime
+        end;
+    end;
 end;
