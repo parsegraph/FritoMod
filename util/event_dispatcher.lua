dbadf32d49941269652d3e43de4514018102cfe9
Aaron Faanes <dafrito@fritocomp.(none)>
2008-08-29 02:16:19 -0500

    Whitespace fixes in event_dispatcher

--- util/event_dispatcher.lua
+++ util/event_dispatcher.lua
@@ -8,30 +8,30 @@ local EventDispatcher = EventDispatcher;
 -------------------------------------------------------------------------------
 
 function EventDispatcher:TriggerEvent(eventName, ...)
-	if not self.events then
-		return;
-	end;
-  local eventList = self.events[eventName];
-  if not eventList then
-    return;
-  end;
-  for listener in eventList:Iter() do
-    listener(eventName, ...);
-  end;
+    if not self.events then
+        return;
+    end;
+    local eventList = self.events[eventName];
+    if not eventList then
+        return;
+    end;
+    for listener in eventList:Iter() do
+        listener(eventName, ...);
+    end;
 end;
 
 function EventDispatcher:AddListener(eventName, listenerFunc, listenerSelf, ...)
 	if not self.events then
 		self.events = {};
 	end;
-  local eventList = self.events[eventName];
-  if not eventList then
-    eventList = List:new();
-    self.events[eventName] = eventList;
-  end;
-  local listener = ObjFunc(listenerFunc, listenerSelf, ...);
-  eventList:Add(listener);
-  return function()
-    eventList:Remove(listener);
-  end;
+    local eventList = self.events[eventName];
+    if not eventList then
+        eventList = List:new();
+        self.events[eventName] = eventList;
+    end;
+    local listener = ObjFunc(listenerFunc, listenerSelf, ...);
+    eventList:Add(listener);
+    return function()
+        eventList:Remove(listener);
+    end;
 end;
