daa02fc662a32f11369d5c0d6b6d7da99cac3725
Aaron Faanes <dafrito@fritocomp.(none)>
2008-08-30 13:57:25 -0500

    Whitespace corrections in data_registry

--- managers/data_registry.lua
+++ managers/data_registry.lua
@@ -1,11 +1,11 @@
 DataRegistry = {
-  listeners=List:new(),
-  dataProviders=List:new(),
+    listeners     = List:new(),
+    dataProviders = List:new(),
 };
 local DataRegistry = DataRegistry;
 
 DataRegistry.listeners.DoRemove = function(listener)
-  listener:DetachAll();
+    listener:DetachAll();
 end;
 
 -------------------------------------------------------------------------------
@@ -15,18 +15,18 @@ end;
 -------------------------------------------------------------------------------
 
 local function AttemptDataProviderConnection(dataProvider, listener)
-	for tagName, tagValue in listener:IterTags() do
-		if dataProvider:GetTag(tagName) ~= tagValue then
-			return;
+    for tagName, tagValue in listener:IterTags() do
+        if dataProvider:GetTag(tagName) ~= tagValue then
+            return;
     end;
-	end;
-	listener:Attach(dataProvider);
+    end;
+    listener:Attach(dataProvider);
 end
 
 local function ConnectDataProvider(self, dataProvider)
-	for listener in self:IterListeners() do
-		AttemptDataProviderConnection(dataProvider, listener);
-	end;
+    for listener in self:IterListeners() do
+        AttemptDataProviderConnection(dataProvider, listener);
+    end;
 end;
 
 -- function DataRegistry:RegisterDataProvider(dataProvider)
@@ -38,32 +38,32 @@ end;
 -- This function is idempotent.
 
 function DataRegistry:RegisterDataProvider(dataProvider)
-	for tagName, _ in dataProvider:IterTags() do
-		local providers = self.dataProviders[tagName];
-		if not providers then
-			providers = List:new();
-			self.dataProviders[tagName] = providers;
-		end;
-		if providers:Add(dataProvider) then
+    for tagName, _ in dataProvider:IterTags() do
+        local providers = self.dataProviders[tagName];
+        if not providers then
+            providers = List:new();
+            self.dataProviders[tagName] = providers;
+        end;
+        if providers:Add(dataProvider) then
       -- It was successfully added, so connect it to all registered listeners who will
       -- accept it. Also add a listener so that when its END event is fired, it is automatically
       -- unregistered.
-			ConnectDataProvider(self, dataProvider);
+            ConnectDataProvider(self, dataProvider);
       dataProvider:AddListener(DataProvider.events.END, "UnregisterDataProvider", self);
-		end;
-	end;
+        end;
+    end;
 end;
 
 -- function DataRegistry:UnregisterDataProvider(dataProvider)
 -- Removes a DataProvider from our registry, assuming one is found.
 -- This will not remove any listeners that have been added to it.
 function DataRegistry:UnregisterDataProvider(dataProvider)
-	for tagName in dataProvider:IterTags() do
-		local providers = self.dataProviders[tagName];
-		if providers then
-			providers:Remove(dataProvider);
-		end;
-	end;
+    for tagName in dataProvider:IterTags() do
+        local providers = self.dataProviders[tagName];
+        if providers then
+            providers:Remove(dataProvider);
+        end;
+    end;
 end;
 
 -------------------------------------------------------------------------------
@@ -80,37 +80,37 @@ end;
 -- a function and are positive it does not already exist.
 
 function DataRegistry:AddListener(tags, listenerFunc, listenerSelf)
-  listener = DataListener:new(tags, listenerFunc, listenerSelf)
-	self.listeners:Add(listener);
-  local index = self.listeners:Length();
-	-- A listener was added, so send it all the DataProviders it would like to listen to.
-	local candidates;
-	for tagName, tagValue in listener:IterTags() do
-		if not candidates then
-      local providers = self.dataProviders[tagName];
-      if providers then
-        candidates = List:new();
-        candidates:AddAll(providers:GetValues());
-      end;
-    else
-      debug(candidates:Length(), tagName);
-      -- Remove any candidate DataProviders whose tagValue doesnt match ours.
-      candidates:Filter(function(dataProvider) 
-        return dataProvider:GetTag(tagName) == tagValue;
-      end);
+    listener = DataListener:new(tags, listenerFunc, listenerSelf)
+    self.listeners:Add(listener);
+    local index = self.listeners:Length();
+    -- A listener was added, so send it all the DataProviders it would like to listen to.
+    local candidates;
+    for tagName, tagValue in listener:IterTags() do
+        if not candidates then
+            local providers = self.dataProviders[tagName];
+            if providers then
+                candidates = List:new();
+                candidates:AddAll(providers:GetValues());
+            end;
+        else
+            debug(candidates:Length(), tagName);
+            -- Remove any candidate DataProviders whose tagValue doesnt match ours.
+            candidates:Filter(function(dataProvider) 
+                return dataProvider:GetTag(tagName) == tagValue;
+            end);
+        end;
     end;
-	end;
-  if candidates then
-    for dataProvider in candidates:Iter() do
-      listener:Attach(dataProvider);
+    if candidates then
+        for dataProvider in candidates:Iter() do
+            listener:Attach(dataProvider);
+        end;
     end;
-  end;
-  -- Returns a function to remove this listener.
-  return function()
-    DataRegistry.listeners:RemoveAt(index);
-  end;
+    -- Returns a function to remove this listener.
+    return function()
+        DataRegistry.listeners:RemoveAt(index);
+    end;
 end;
 
 function DataRegistry:IterListeners()
-	return self.listeners:Iter();
+    return self.listeners:Iter();
 end;
