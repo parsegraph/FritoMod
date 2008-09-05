3aa05a9b6af7aea680d1baa21c950266fabf5ebb
Aaron Faanes <dafrito@fritocomp.(none)>
2008-09-05 17:58:39 -0500

    Moved our operators to a separate lua, and standardized their naming convention

---
+++ util/operator.lua
@@ -0,0 +1,16 @@
+Operator = {
+    Equals = function(a, b) return a == b end,
+    NotEquals = function(a, b) return a ~= b end,
+    LessThan = function(a, b) return a < b end,
+    GreaterThan = function(a, b) return a > b end,
+    LessThanOrEqual = function(a, b) return a <= b end,
+    GreaterThanOrEqual = function(a, b) return a >= b end,
+
+    InclusiveRange = function(min, max, num) return min <= num and num <= max end,
+    ExclusiveRange = function(min, max, num) return min < num and num < max end,
+
+    Add = function(a, b) return a + b end,
+    Subtract = function(a, b) return a - b end,
+    Multiply = function(a, b) return a * b end,
+    Divide = function(a, b) return a / b end,
+}
