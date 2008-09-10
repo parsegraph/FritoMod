390f5788644a9c20c94bebbfc8406faf532cccff
Aaron Faanes <dafrito@fritocomp.(none)>
2008-09-09 19:41:55 -0500

    data rewrite

--- util/event_dispatcher.lua
+++ util/event_dispatcher.lua
@@ -1,5 +1,49 @@
-EventDispatcher = AceLibrary("AceOO-2.0").Mixin{"TriggerEvent", "AddListener"};
+EventDispatcher = AceLibrary("AceOO-2.0").Mixin{
+    "TriggerEvent", "AddListener", 
+    "AddEventConnector", "InitEventConnectors", "GetEventConnector"
+};
 local EventDispatcher = EventDispatcher;
+
+function EventDispatcher.MixinStaticEventConnectors(prototype)
+    local connectors = prototype.staticEventConnectors;
+    if not connectors then
+        connectors = {};
+        prototype.staticEventConnectors = connectors;
+    end;
+    prototype["AddStaticEventConnector"] = function(eventName, unappliedFunc, ...)
+        local unappliedFunc = Unapplied(unappliedFunc, ...);
+        connectors[eventName] = unappliedFunc;
+    end;
+    prototype["ApplyStaticEventConnectors"] = function(self)
+        for eventName, unappliedFunc in pairs(connectors) do
+            self:AddConnector(unappliedFunc(self));
+        end;
+    end;
+end;
+
+-------------------------------------------------------------------------------
+--
+--  Connector Methods
+--
+-------------------------------------------------------------------------------
+
+function EventDispatcher:InitEventConnectors()
+    self.prototype.class.ApplyStaticEventConnectors(self);
+end;
+
+function EventDispatcher:AddEventConnector(eventName, connectorFunc, ...)
+    connectorFunc = ObjFunc(connectorFunc, ...);
+    if not self.eventConnectors then 
+        self.eventConnectors = {};
+    end;
+    self.eventConnectors[eventName] = connectorFunc;
+end;
+
+function EventDispatcher:GetEventConnector(eventName)
+    if self.eventConnectors then
+        return self.eventConnectors[eventName];
+    end;
+end;
 
 -------------------------------------------------------------------------------
 --
@@ -24,13 +68,13 @@ function EventDispatcher:AddListener(eventName, listenerFunc, ...)
 	if not self.events then
 		self.events = {};
 	end;
-    local listener = ObjFunc(listenerFunc, ...);
+    listenerFunc = ObjFunc(listenerFunc, ...);
     if type(eventName) == "table" then
         -- We're using the same listener for multiple events, so recurse and collect.
         removers = {}
         local eventList = eventName
         for i, eventName in ipairs(eventList) do
-            table.insert(removers, self:AddListener(eventName, listener))
+            table.insert(removers, self:AddListener(eventName, listenerFunc))
         end
         return function()
             for i, remover in ipairs(removers) do
@@ -43,8 +87,13 @@ function EventDispatcher:AddListener(eventName, listenerFunc, ...)
         eventList = List:new();
         self.events[eventName] = eventList;
     end;
-    eventList:Add(listener);
+    if eventList:Add(listenerFunc) then
+        connectorFunc = self:GetEventConnector(eventName);
+        if connectorFunc then
+            listenerFunc(eventName, connectorFunc());
+        end;
+    end;
     return function()
-        eventList:Remove(listener);
+        eventList:Remove(listenerFunc);
     end;
 end;
