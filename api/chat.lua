2a35e6cf2648ad3df0dcc66e30b264c043e23919
Aaron Faanes <dafrito@fritocomp.(none)>
2008-09-17 16:04:55 -0500

    Added color stuff, and refactored Say/Print

--- api/chat.lua
+++ api/chat.lua
@@ -12,6 +12,7 @@ Chat.mediums = {
     RAID_WARNING = "raid_warning",
     BATTLEGROUND = "battleground",
     DEBUG = "debug",
+    NULL = "null",
 }
 
 Chat.fauxMediums = {
@@ -21,20 +22,62 @@ Chat.fauxMediums = {
     DND = "dnd"
 };
 
-function Chat:Print(message)
-    DEFAULT_CHAT_FRAME:AddMessage(message);
+function Chat:Color(color, message)
+    if color == nil then
+        return message;
+    end;
+    if type(color) == "string" then
+        local retrievedColor = MediaLibrary:GetExplicit("Color", color);
+        if retrievedColor then
+            color = retrievedColor;
+        end;
+    end;
+    if type(color) == "table" then
+        color = Chat:GetColorString(color);
+    end;
+    if #color ~= 8 or not string.find(color, "^[0-9a-fA-F]+$") then
+        error(format("Color is invalid: '%s'", color));
+    end;
+    return "|c" .. color .. message .. "|r";
 end;
 
-function Chat:Say(medium, message, language)
+function Chat:GetColorString(color, ...)
+    if select("#", ...) > 0 then
+        color = { color, ... };
+    end;
+    return string.join("", unpack(ListUtil:Map(color, {}, function(colorValue)
+        local hexColor = ConvertToBase(16, math.floor(colorValue * 255));
+        while #hexColor < 2 do
+            hexColor = "0" .. hexColor;
+        end;
+        return hexColor;
+    end)));
+end;
+
+function Chat:Print(message)
+    return Chat:Say(Chat.mediums.DEBUG, message);
+end;
+
+function Chat:Say(medium, message, language, color, ...)
+    if select("#", ...) > 0 then
+        color = {color, ...};
+    end;
     if not language then
         language = API.Unit:GetDefaultLanguage("player");
     end;
     if not medium or type(medium) ~= "string" then
-        error("Invalid medium");
+        error("Invalid medium (Medium is falsy)");
     end;
     medium = string.upper(medium);
-    if string.lower(medium) == Chat.mediums.DEBUG then
-        return API.Chat:Print(message);
+    local loweredMedium = string.lower(medium);
+    if loweredMedium == Chat.mediums.NULL or loweredMedium == Chat.mediums.DEBUG then
+        if color then
+            message = API.Chat:Color(color, message);
+        end;
+        if loweredMedium == Chat.mediums.DEBUG then
+            DEFAULT_CHAT_FRAME:AddMessage(message);
+        end;
+        return message;
     end;
     if Chat.mediums[medium] then
         SendChatMessage(message, medium, language);
@@ -46,6 +89,7 @@ function Chat:Say(medium, message, language)
         end;
         SendChatMessage(message, Chat.fauxMediums.CHANNEL, language, channel);
     end;
+    return message;
 end;
 
 function Chat:Whisper(playerName, message, language)
@@ -62,5 +106,5 @@ end;
 
 function Chat:GetChannels()
     local channels = { GetChannelList() };
-
+    error("NYI");
 end;
