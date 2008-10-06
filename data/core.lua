e04bf0da2e7fc87f9ca272439fc71264b2b7dafd
Aaron Faanes <dafrito@fritocomp.(none)>
2008-10-06 04:01:04 -0500

    namechange: data.core

--- data/core.lua
+++ data/core.lua
@@ -1,89 +1,83 @@
 Data = {};
 local Data = Data;
 
-function Data:MixinScalar(class, name, lowerName, upperName)
-    if not lowerName then
-        lowerName = string.lower(name);
-    end;
-    if not upperName then
-        upperName = string.upper(name);
-    end;
+function Data:MixinScalar(class, properName, lowerName, upperName)
+    lowerName = lowerName or string.lower(properName);
+    upperName = upperName or string.upper(properName);
     local eventName = upperName .. "_UPDATE";
-    class["Get" .. name] = function(self)
+    class["Get" .. properName] = function(self)
         return self[lowerName];
     end;
-    class["Set" .. name] = function(self, value)
+    class["Set" .. properName] = function(self, value)
         if self[lowerName] == value then
             return;
         end;
         self[lowerName] = value;
-        self:TriggerEvent(eventName, value);
+        self:DispatchEvent(eventName, value);
     end;
-    -- These event connectors are Unapplied, since they have no current
-    -- 'self' they can immediately refer to.
     class:AddStaticEventConnector(eventName, function(self, eventName, listenerFunc)
-        listenerFunc(self["Get" .. name](self));
+        listenerFunc(eventName, self["Get" .. properName](self));
     end);
 end;
 
-function Data:MixinRange(class, name, lowerName, upperName)
+function Data:MixinRange(class, properName, lowerName, upperName)
     if not lowerName then
-        lowerName = string.lower(name);
+        lowerName = string.lower(properName);
     end;
     if not upperName then
-        upperName = string.upper(name);
+        upperName = string.upper(properName);
     end;
     local currentValue, minValue, maxValue, eventName = 
-        "current" .. name, 
-        "min" .. name, 
-        "max" .. name,
+        "current" .. properName, 
+        "min" .. properName, 
+        "max" .. properName,
         upperName .. "_UPDATE";
-    class["Get" .. name] = function(self)
-        return self["GetCurrent" .. name](self),
-            self["GetMin" .. name](self),
-            self["GetMax" .. name](self);
+    class["Get" .. properName] = function(self)
+        return self["GetCurrent" .. properName](self),
+            self["GetMin" .. properName](self),
+            self["GetMax" .. properName](self);
     end;
-    class["GetCurrent" .. name] = function(self)
+    class["GetCurrent" .. properName] = function(self)
         return self[currentValue];
     end;
-    class["GetMin" .. name] = function(self)
+    class["GetMin" .. properName] = function(self)
         return self[minValue];
     end;
-    class["GetMax" .. name] = function(self)
+    class["GetMax" .. properName] = function(self)
         return self[maxValue];
     end;
-    class["Set" .. name] = function(self, current, max, min)
-        local oldCurrent, oldMin, oldMax = self["Get" .. name](self);
+    class["Set" .. properName] = function(self, current, max, min)
+        local oldCurrent, oldMin, oldMax = self["Get" .. properName](self);
         if oldCurrent == current and oldMin == min and oldMax == max then
             return;
         end;
         self[currentValue] = current;
         self[minValue] = min;
         self[maxValue] = max;
-        self:TriggerEvent(eventName, current, max, min);
+        self:DispatchEvent(eventName, current, max, min);
     end;
-    class["SetCurrent" .. name] = function(self, current)
-        return self["Set" .. name](self, 
+    class["SetCurrent" .. properName] = function(self, current)
+        return self["Set" .. properName](self, 
             current, 
-            self["GetMin" .. name](self), 
-            self["GetMax" .. name](self)
+            self["GetMin" .. properName](self), 
+            self["GetMax" .. properName](self)
         );
     end;
-    class["SetMin" .. name] = function(self, min)
-        return self["Set" .. name](self, 
-            self["GetCurrent" .. name](self), 
+    class["SetMin" .. properName] = function(self, min)
+        return self["Set" .. properName](self, 
+            self["GetCurrent" .. properName](self), 
             min,
-            self["GetMax" .. name](self)
+            self["GetMax" .. properName](self)
         );
     end;
-    class["SetCurrent" .. name] = function(self, max)
-        return self["Set" .. name](self, 
-            self["GetCurrent" .. name](self), 
-            self["GetMin" .. name](self), 
+    class["SetCurrent" .. properName] = function(self, max)
+        return self["Set" .. properName](self, 
+            self["GetCurrent" .. properName](self), 
+            self["GetMin" .. properName](self), 
             max
         );
     end;
     class:AddStaticEventConnector(eventName, function(self, eventName, listenerFunc)
-        listenerFunc(self["Get" .. name](self));
+        listenerFunc(eventName, self["Get" .. properName](self));
     end);
 end;
