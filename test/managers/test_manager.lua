82fa808fff51b2d6de37d47804ffde910e1e6cf0
Aaron Faanes <dafrito@fritocomp.(none)>
2008-10-06 04:03:11 -0500

    test cases now lazier

--- test/managers/test_manager.lua
+++ test/managers/test_manager.lua
@@ -1,44 +1,46 @@
-local testManager = TestManager:GetInstance();
+Environment:AddBootstrapper(Environment.runLevels.DEPLOY_CORE, function()
+    local testManager = TestManager:GetInstance();
 
-testManager:InsertTestCase("managers.testManager", TestCase("InsertTestCase",
-    TestCase.returnTypes.CONSTANT, true,
-    function()
-        return true;
-    end
-));
+    testManager:InsertTestCase("managers.testManager", TestCase("InsertTestCase",
+        TestCase.returnTypes.CONSTANT, true,
+        function()
+            return true;
+        end
+    ));
 
-testManager:AddTest("managers.TestManager", "Sanity Check - Constant Test", 
-    TestCase.returnTypes.CONSTANT, true, 
-    function()
-        return true;
-    end
-);
+    testManager:AddTest("managers.TestManager", "Sanity Check - Constant Test", 
+        TestCase.returnTypes.CONSTANT, true, 
+        function()
+            return true;
+        end
+    );
 
-testManager:AddTest("managers.TestManager", "Sanity Check - Exception Test", 
-    TestCase.returnTypes.EXCEPTION, "An exception is raised",
-    function()
-        error("An exception is raised");
-    end
-);
+    testManager:AddTest("managers.TestManager", "Sanity Check - Exception Test", 
+        TestCase.returnTypes.EXCEPTION, "An exception is raised",
+        function()
+            error("An exception is raised");
+        end
+    );
 
-testManager:AddTest("managers.TestManager", "Sanity Check - Complex Test",
-    TestCase.returnTypes.COMPLEX,
-    function(a, b, c)
-        return a == "a" and b == "b" and c == "c";
-    end,
-    function()
-        return "a", "b", "c";
-    end
-);
+    testManager:AddTest("managers.TestManager", "Sanity Check - Complex Test",
+        TestCase.returnTypes.COMPLEX,
+        function(a, b, c)
+            return a == "a" and b == "b" and c == "c";
+        end,
+        function()
+            return "a", "b", "c";
+        end
+    );
 
-testManager:AddTest("managers.TestManager", "Sanity Check - Clean function call in test cases",
-    TestCase.returnTypes.CONSTANT, 0, 
-    function(...)
-        return select("#", ...);
-    end
-);
+    testManager:AddTest("managers.TestManager", "Sanity Check - Clean function call in test cases",
+        TestCase.returnTypes.CONSTANT, 0, 
+        function(...)
+            return select("#", ...);
+        end
+    );
 
-testManager:AddTest("managers.TestManager", "Sanity Check - Partial'd function test case",
-    TestCase.returnTypes.CONSTANT, "string",
-    type, "This is a string."
-);
+    testManager:AddTest("managers.TestManager", "Sanity Check - Partial'd function test case",
+        TestCase.returnTypes.CONSTANT, "string",
+        type, "This is a string."
+    );
+end);
