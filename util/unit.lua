1490f045fbd3d1646f4a0c2557c4bf1bbe61d32d
Aaron Faanes <dafrito@fritocomp.(none)>
2008-10-06 03:57:16 -0500

    util.unit now lazier

--- util/unit.lua
+++ util/unit.lua
@@ -1,13 +1,15 @@
 Unit = OOP.Class(EventDispatcher)
 local Unit = Unit;
 
-Data:MixinScalar("Name");
-Data:MixinScalar("Faction");
-Data:MixinScalar("Realm");
-Data:MixinScalar("Class");
-Data:MixinScalar("Race");
-Data:MixinScalar("Gender");
-Data:MixinRange("Health");
+Unit.__AddInitializer(function(class)
+    Data:MixinScalar(class, "Name");
+    Data:MixinScalar(class, "Faction");
+    Data:MixinScalar(class, "Realm");
+    Data:MixinScalar(class, "Class");
+    Data:MixinScalar(class, "Race");
+    Data:MixinScalar(class, "Gender");
+    Data:MixinRange(class, "Health");
+end);
 
 function Unit:LoadBase(name, realm, faction, class, race, gender)
     self:SetName(name);
