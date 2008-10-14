5f898ac9c9fd5ffc812469ac5df6ee2421f57cfe
Aaron Faanes <dafrito@fritocomp.(none)>
2008-10-14 09:05:34 -0500

    Major cleanup in string (added real converters between case-styles)

--- util/string.lua
+++ util/string.lua
@@ -5,9 +5,123 @@ StringUtil.DIGITS = "0123456789"
 StringUtil.ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 StringUtil.ALPHANUMERICS = StringUtil.DIGITS .. StringUtil.ALPHABET;
 
-function StringUtil:ProperNounize(name)
-    name = tostring(name);
-    return strupper(strsub(name, 1, 1)) .. strlower(strsub(name, 2));
+-------------------------------------------------------------------------------
+--
+--  Naming Conversion Functions
+--
+-------------------------------------------------------------------------------
+--
+-- These functions allow intelligent conversion from ProperCase, snake_case, and
+-- camelCase.
+--
+-- Observe that converting back and forth may sometimes be destructive. As a convention, 
+-- don't convert from a previously converted string. If you have to, be sure that either 
+-- the names don't contain potentially-harmful parts, or that yielding non-identical results
+-- during multiple conversions will be harmless.
+--
+-- You'll avoid trouble for the most part by avoiding acronyms in your strings. For
+-- example, 'aFooAPI' will be converted into 'a_foo_api', but converting 'a_foo_api'
+-- back to camelCase yields 'aFooApi'. Be very aware of these differences when doing
+-- your conversions.
+--
+-- Also be aware that these functions are _NOT_ idempotent. Converting 'a_foo_api' to
+-- snake_case again will yield unpredictable results.
+
+------------------------------------------
+--  SplitByCase
+------------------------------------------
+-- 
+-- Splits a camelCase'd or ProperCase'd string into lower-case words. Acronyms will be 
+-- treated as single words.
+--
+-- Observe that camelCase's passed into this method will be parsed correctly; the list
+-- returned will be {"camel", "Case"}
+function StringUtil:SplitByCase(originalString)
+    originalString = tostring(originalString);
+    local words = {};
+    string.gsub(originalString, "^(%l-)(%U*)$", function(initial, rest)
+        table.insert(words, initial);
+        originalString = rest;
+    end)
+    string.gsub(originalString , "(%U.*%l*)%U%l", function(word)
+        table.insert(words, string.lower(word));
+    end);
+    table.insert(words, word);
+    return words;
+end;
+
+------------------------------------------
+--  SplitByDelimiter
+------------------------------------------
+-- 
+-- Splits originalString by the given delimiter, with an underscore used as the default.
+function StringUtil:SplitByDelimiter(originalString, delimiter)
+    delimiter = delimiter or "_";
+    return string.split(delimiter, originalString);
+end;
+
+------------------------------------------
+--  String Joiners
+------------------------------------------
+
+function StringUtil:JoinProperCase(words)
+    return ListUtil:Reduce(words, "", function(camelCase, word)
+        return camelCase + StringUtil:ProperNounize(word);
+    end);
+end;
+
+function StringUtil:JoinCamelCase(words)
+    local properString = StringUtil:JoinProperCase(words);
+    return string.lower(string.sub(properString, 1, 1)) .. string.sub(properString, 2);
+end;
+
+function StringUtil:JoinSnakeCase(words)
+    return string.join("_", words);
+end;
+
+------------------------------------------
+--  ProperCase Converters
+------------------------------------------
+
+-- Converts AProperCase to aProperCase. 
+function StringUtil:ProperToCamelCase(properString)
+    return StringUtil:JoinCamelCase(StringUtil:SplitByCase(properString));
+end;
+
+-- Converts AProperCase to a_proper_case.
+function StringUtil:ProperToSnakeCase(properString)
+    return StringUtil:JoinSnakeCase(StringUtil:SplitByCase(properString));
+end;
+
+------------------------------------------
+--  camelCase Converters
+------------------------------------------
+
+-- Converts aProperCase to AProperCase. 
+function StringUtil:CamelToProperCase(camelString)
+    return StringUtil:JoinProperCase(StringUtil:SplitByCase(camelString));
+end;
+
+-- Converts aProperCase to a_proper_case.
+function StringUtil:CamelToSnakeCase(camelString)
+    return StringUtil:JoinSnakeCase(StringUtil:SplitByCase(camelString));
+end;
+
+------------------------------------------
+--  snake_case Converters
+------------------------------------------
+
+function StringUtil:SnakeToProperCase(snakeString)
+    return StringUtil:JoinProperCase(StringUtil:SplitByDelimiter(snakeString));
+end;
+
+function StringUtil:SnakeToCamelString(snakeString)
+    return StringUtil:JoinCamelCase(StringUtil:SplitByDelimiter(snakeString));
+end;
+
+function StringUtil:ProperNounize(word)
+    word = tostring(word);
+    return string.upper(string.sub(word, 1, 1)) .. string.lower(string.sub(word, 2));
 end;
 
 function StringUtil:ConvertToBase(base, number, digits)
@@ -25,15 +139,10 @@ function StringUtil:ConvertToBase(base, number, digits)
 end
 
 function StringUtil:Concat(...)
-	local message = "";
-	for i=1, select("#", ...) do
-        local part = tostring(select(i, ...));
-        if #message > 0 then
-            message = format("%s %s", message, part);
-        else
-            message = part;
+    return ListUtil:Reduce({...}, "", function(concatted, word)
+        if #concatted > 0 then
+            return concatted .. " " .. word;
         end;
-	end
-	return message;
+        return word;
+    end);
 end;
-
