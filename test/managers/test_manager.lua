4cdef7f2f2fe6e0a5018c6b1b7c78053580da349
Aaron Faanes <dafrito@fritocomp.(none)>
2008-10-15 02:58:50 -0500

    Added howto for test-case and streamlined TestManager's tests a bit

--- test/managers/test_manager.lua
+++ test/managers/test_manager.lua
@@ -1,28 +1,65 @@
+-------------------------------------------------------------------------------
+--
+--  Test Cases: How-To
+--
+-------------------------------------------------------------------------------
+--
+-- Setting up a test-suite involves creating and adding test-cases. You can do
+-- this using AddTest for the given TestManager instance.
+--
+-- AddTest(testGroupName, testCaseName, expectedReturnType, expectedReturnValue, testFunc, ...)
+-- 
+-- These are following returnTypes available:
+-- TestCase.returnTypes = {
+--     CONSTANT = "constant",
+--     COMPLEX = "complex",
+--     EXCEPTION = "exception",
+-- };
+--
+-- A constant means you should give the exact value you expect.
+-- An exception means you should give the exact exception you expect thrown.
+-- A complex value means you should give a function that will be given the results. Notice
+-- that complex values cannot be given exceptions.
+--
+-- The common idiom is to register this at DEPLOY_CORE stage with the Environment.
+-- When in doubt, just use this as a template. Be sure to retain these instructions
+-- so you can dessiminate this process!)
+
 Environment:AddBootstrapper(Environment.runLevels.DEPLOY_CORE, function()
     local testManager = TestManager:GetInstance();
+    local testGroup = "managers.testManager";
 
-    testManager:InsertTestCase("managers.testManager", TestCase("InsertTestCase",
+    -- Utility method to add cases.
+    function Add(testCaseOrName, ...)
+        local testCase = testCaseOrName;
+        if type(testCaseOrName) ~= "table" then
+            testCase = TestCase(testCaseOrName, ...);
+        end;
+        testManager:InsertTestCase(testGroup, testCase);
+    end;
+
+    Add("InsertTestCase", 
         TestCase.returnTypes.CONSTANT, true,
         function()
             return true;
         end
-    ));
+    );
 
-    testManager:AddTest("managers.TestManager", "Sanity Check - Constant Test", 
+    Add("Sanity Check - Constant Test", 
         TestCase.returnTypes.CONSTANT, true, 
         function()
             return true;
         end
     );
 
-    testManager:AddTest("managers.TestManager", "Sanity Check - Exception Test", 
+    Add("Sanity Check - Exception Test", 
         TestCase.returnTypes.EXCEPTION, "An exception is raised",
         function()
             error("An exception is raised");
         end
     );
 
-    testManager:AddTest("managers.TestManager", "Sanity Check - Complex Test",
+    Add("Sanity Check - Complex Test",
         TestCase.returnTypes.COMPLEX,
         function(a, b, c)
             return a == "a" and b == "b" and c == "c";
@@ -32,15 +69,16 @@ Environment:AddBootstrapper(Environment.runLevels.DEPLOY_CORE, function()
         end
     );
 
-    testManager:AddTest("managers.TestManager", "Sanity Check - Clean function call in test cases",
+    Add("Sanity Check - Clean function call in test cases",
         TestCase.returnTypes.CONSTANT, 0, 
         function(...)
             return select("#", ...);
         end
     );
 
-    testManager:AddTest("managers.TestManager", "Sanity Check - Partial'd function test case",
+    Add("Sanity Check - Partial'd function test case",
         TestCase.returnTypes.CONSTANT, "string",
         type, "This is a string."
     );
+
 end);
