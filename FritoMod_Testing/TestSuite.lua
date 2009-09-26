0828970e216b75cd27aeff9ad41263c52c0dda0b
Aaron Faanes <dafrito@gmail.com>
2009-09-26 08:53:43 -0500

    [see #4] TestSuite cleanup
    * TestSuite is now much more flexible in the types of tests it can
    run. Executable functions, functions, other TestSuites are all
    acceptable.
    * TestSuite can now be transparently composed of other TestSuites.
    This lays the groundwork for a global TestSuite, and removes the
    need for an explicit TestManager or TestRegistry class. We will
    likely still have one, but this makes TestSuites much more flexible
    and independent

--- FritoMod_Testing/TestSuite.lua
+++ FritoMod_Testing/TestSuite.lua
@@ -9,21 +9,45 @@ function TestSuite:AddListener(listenerFunc, ...)
     return self.listener:Add(listenerFunc, ...);
 end;
 
+local function RunTest(self, test)
+    if not test then
+        return false, "Test is falsy";
+    end;
+    if IsCallable(test) then
+        return pcall(test);
+    end;
+    if type(test) == "table" then
+        return pcall(test.Run, test);
+    end;
+    if type(test) == "string" then
+        local testfunc, err = loadstring(test);
+        if testFunc then
+            return RunTest(self, testFunc);
+        end;
+        return false, err;
+    end;
+    return false, "Test is not a callable, table, or string: " .. type(test);
+end;
+
 function TestSuite:Run()
     self.listener:StartAllTests(self);
     local tests = self:GetTests();
     if type(tests) == "function" then
         tests = Lists.Consume(tests);
     end;
+    local successful = true;
     for test in ipairs(tests) do
         self.listener:TestStarted(self, test);
-        if pcall(test) then
+        local result, errorMessage = RunTest(self, test);
+        if result then
             self.listener:TestSuccessful(self, test);
         else
-            self.listener:TestFailed(self, test);
+            successful = false;
+            self.listener:TestFailed(self, test, errorMessage);
         end;
     end;
     self.listener:FinishAllTests(self);
+    return successful;
 end;
 
 function TestSuite:GetTests()
