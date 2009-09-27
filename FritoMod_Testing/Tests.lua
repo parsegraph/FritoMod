efb829a2a039feabc3851fa1d78e5e1dbe37f315
Aaron Faanes <dafrito@gmail.com>
2009-09-27 07:37:51 -0500

    [see #4] Tests.lua now contains some utility functions for testing
    This means that Flag is no longer a global function.

--- FritoMod_Testing/Tests.lua
+++ FritoMod_Testing/Tests.lua
@@ -1,9 +1,12 @@
+Tests = {};
+local Tests = Tests;
+
 -- A very simple class that acts as a boolean object. Useful in testing since
 -- it provides explicit methods that can be curried.
 --
 -- This class' methods operate through closure, so they may be invoked directly;
 -- the 'self' reference is not used.
-function Flag()
+function Tests.Flag()
     local isSet = false;
     local flag = {
         Raise = function()
@@ -17,4 +20,17 @@ function Flag()
         assert(self:IsSet(), ...);
     end);
     return flag;
+end
+
+function Tests.Counter()
+    local count = 0;
+    local counter = {
+        Hit = function()
+            count = count + 1;
+        end,
+        Count = function()
+            return count;
+        end,
+    };
+    return counter;
 end;
