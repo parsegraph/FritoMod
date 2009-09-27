4afe40f3d97a4eb8b50aec918f431089805d3495
Aaron Faanes <dafrito@gmail.com>
2009-09-27 15:20:12 -0500

    [new] IsClass, IsInstance, InstanceOf

--- FritoMod_OOP/OOP/methods.lua
+++ FritoMod_OOP/OOP/methods.lua
@@ -3,10 +3,38 @@ local OOP = OOP;
 -- Integrates a library into a given class. This takes all
 -- public (non-double-underscored functions), and adds them
 -- to the specified class.
-OOP.IntegrateLibrary = function(library, class)
+function OOP.IntegrateLibrary(library, class)
     for funcName, func in pairs(library) do
         if not string.match(funcName, "^__") then
             class[funcName] = func;
         end;
     end
 end
+
+function OOP.InstanceOf(class, instance)
+    assert(OOP.IsClass(class), "class is not a class");
+    if not OOP.IsInstance(instance) then
+        return false;
+    end;
+    local candidateClass = instance.class;
+    while true do
+        if candidateClass == class then
+            return true;
+        end;
+        if candidateClass.super ~= candidateClass then
+            candidateClass = candidateClass.super;
+        else
+            break;
+        end;
+    end;
+    return false;
+end;
+
+function OOP.IsInstance(candidate)
+    return candidate and type(candidate) == "table" and OOP.IsClass(candidate.class);
+end;
+
+function OOP.IsClass(candidate)
+    return candidate and type(candidate) == "table" and type(candidate.New) == "function";
+end;
+
