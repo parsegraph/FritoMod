6bf9d0f8ed1ca186f194aecaa211df294fd78792
Aaron Faanes <dafrito@gmail.com>
2009-09-29 10:55:59 -0500

    [addresses #5] Lists now has a lot of test stubs

--- FritoMod_Utilities_Tests/Lists.lua
+++ FritoMod_Utilities_Tests/Lists.lua
@@ -1,5 +1,113 @@
-local testManager = TestManager:GetInstance();
-local releaser = testManager:SetActiveTestGroup("FritoMod_Utilities.ListUtil");
+local ListsTests = ReflectiveTestSuite:New("FritoMod_Utilities.lists");
 
+function ListsTests:TestContainsAll()
 
-releaser();
+end;
+
+function ListsTests:TestEquals()
+
+end;
+
+function ListsTests:TestIndexOf()
+
+end;
+
+function ListsTests:TestLastIndexOf()
+
+end;
+
+function ListsTests:TestReduce()
+
+end;
+
+function ListsTests:TestUpdate()
+
+end;
+
+function ListsTests:TestClone()
+
+end;
+
+function ListsTests:TestInsert()
+
+end;
+
+function ListsTests:TestInsertAll()
+
+end;
+
+function ListsTests:TestInsertAt()
+
+end;
+
+function ListsTests:TestInsertAllAt()
+
+end;
+
+function ListsTests:TestClear()
+
+end;
+
+function ListsTests:TestConsume()
+
+end;
+
+function ListsTests:Join()
+
+end;
+
+function ListsTests:TestMapInPlace()
+
+end;
+
+function ListsTests:TestMap()
+
+end;
+
+function ListsTests:TestMapCall()
+
+end;
+
+function ListsTests:TestFilter()
+
+end;
+
+function ListsTests:TestFilterInPlace()
+
+end;
+
+function ListsTests:TestFilter()
+    
+end;
+
+function ListsTests:TestFilterIndex()
+
+end;
+
+function ListsTests:TestRemoveFirst()
+
+end;
+
+function ListsTests:TestRemoveLast()
+
+end;
+
+function ListsTests:TestRemoveAll()
+
+end;
+
+function ListsTests:TestReverse()
+
+end;
+
+function ListsTests:TestReverseInPlace()
+
+end;
+
+function ListsTests:TestDifference()
+
+end;
+
+function ListsTests:TestUnion()
+
+end;
