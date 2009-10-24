b099012e7a7e67f7403a7fbe8a2ca2f437b6b7ca
Aaron Faanes <dafrito@gmail.com>
2009-10-24 00:08:49 -0500

    [move] Removed Utilities
    * Metatables has returned to Functional
    * Math is now in its own project

--- FritoMod_Utilities_Tests/Math.lua
+++
@@ -1,7 +0,0 @@
-local MathTests = ReflectiveTestSuite:New("FritoMod_Utilities.Math");
-
-function MathTests:TestParseTime()
-    local p = Math.Parse;
-    Assert.Equals(1, p("one"));
-    Assert.Equals(2, p("two"));
-end;
