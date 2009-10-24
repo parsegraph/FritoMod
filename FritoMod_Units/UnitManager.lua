e9719ea9a26519572a7e00b6028bc1b618052909
Aaron Faanes <dafrito@gmail.com>
2009-10-24 00:42:00 -0500

    Removed reference to API project in unitManager

--- FritoMod_Units/UnitManager.lua
+++ FritoMod_Units/UnitManager.lua
@@ -4,10 +4,10 @@ UnitManager = {
 local UnitManager = UnitManager;
 
 function UnitManager:FetchByID(unitID, fetch_only)
-    return self:Fetch(API.Unit:GetGUID(unitID), fetch_only);
+    return self:Fetch(UnitGUID(unitID), fetch_only);
 end;
 
 function UnitManager:Fetch(unitID)
-    local guid = API.Unit:GetGUID(unitID);
+    local guid = UnitGUID(unitID);
     return self.registry[guid]
 end;
