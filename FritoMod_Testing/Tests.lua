5253481a57c2e39f92e7845ae2583393de9a0507
Aaron Faanes <dafrito@gmail.com>
2009-10-25 07:33:55 -0500

    [new] AddErrorListener function, Modifications to Flag
    * Flag now has a Clear method
    * AddErrorListener allows listeners to listen to the stack trace
    when errors propagate

--- FritoMod_Testing/Tests.lua
+++ FritoMod_Testing/Tests.lua
@@ -1,6 +1,33 @@
 Tests = {};
 local Tests = Tests;
 
+do
+    local listeners = {};
+    Tests.AddErrorListener = Activator(FunctionPopulator(listeners), function()
+        local oldHandler = geterrorhandler() or Noop;
+        function OurHandler(errorMessage, frame, stack, etype, ...)
+            print("Cpaturing?");
+            seterrorhandler(oldHandler);
+            -- We unhook our handler in case one of *our* handlers fails.
+            pcall(Lists.CallEach, listeners, errorMessage, etype, stack, ...);
+            seterrorhandler(OurHandler);
+            oldHandler();
+        end;
+        seterrorhandler(OurHandler);
+        return Curry(seterrorhandler, oldHandler);
+    end);
+end;
+
+function Tests.Choke(choke)
+    local count = 0;
+    return function()
+        count = count + 1;
+        if count > choke then
+            error("Choked at count: " ..count);
+        end;
+    end;
+end;
+
 -- A very simple class that acts as a boolean object. Useful in testing since
 -- it provides explicit methods that can be curried.
 --
@@ -14,6 +41,9 @@ function Tests.Flag()
         end,
         IsSet = function()
             return isSet;
+        end,
+        Clear = function()
+            isSet = false;
         end
     };
     flag.Assert = ForcedMethod(flag, function(self, ...)
