ed69db1dcbda4e14c00c083b8ec0424c5162b002
Aaron Faanes <dafrito@gmail.com>
2009-10-26 12:02:40 -0500

    [new test] Some new tests for currying

---
+++ FritoMod_Functional_Tests/currying.lua
@@ -0,0 +1,35 @@
+local Suite = ReflectiveTestSuite:New("FritoMod_Functional.currying");
+
+function Suite:TestForcedFunctionOnNoop()
+    local foo = {};
+    foo.bar = ForcedFunction(foo, function(value)
+        Assert.Equals(1, value, "Value was passed appropriately");
+    end);
+    foo.bar(1);
+end;
+
+function Suite:TestForcedFunction()
+    local foo = {};
+    foo.bar = ForcedFunction(foo, function(value)
+        Assert.Equals(1, value, "Value was passed appropriately");
+    end);
+    foo:bar(1);
+end;
+
+function Suite:TestForcedMehodOnNoop()
+    local foo = {};
+    foo.bar = ForcedMethod(foo, function(self, value)
+        Assert.Equals(foo, self, "Self argument was passed appropriately");
+        Assert.Equals(1, value, "Value was passed appropriately");
+    end);
+    foo:bar(1);
+end;
+
+function Suite:TestForcedFunction()
+    local foo = {};
+    foo.bar = ForcedMethod(foo, function(self, value)
+        Assert.Equals(foo, self, "Self argument was passed appropriately");
+        Assert.Equals(1, value, "Value was passed appropriately");
+    end);
+    foo.bar(1);
+end;
