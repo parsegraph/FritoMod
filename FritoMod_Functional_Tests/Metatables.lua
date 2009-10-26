69d6eed4d793d46946b725dcf522ca0f8410e723
Aaron Faanes <dafrito@gmail.com>
2009-10-25 22:49:50 -0500

    [delete test] Removed duplicate test in Functional.Metatables

--- FritoMod_Functional_Tests/Metatables.lua
+++ FritoMod_Functional_Tests/Metatables.lua
@@ -1,23 +1,5 @@
 local MetatableTests = ReflectiveTestSuite:New("FritoMod_Utilities.Metatables");
 
-function MetatableTests:TestMulticast()
-    local comp = Metatables.Multicast();
-
-    local x = Tests.Flag();
-    local y = Tests.Flag();
-
-    comp:Add(x);
-    comp:Raise();
-
-    x:Assert("X has been raised");
-
-    comp:Add(y);
-    comp:Raise();
-
-    x:Assert("X and Y have been raised");
-    y:Assert("X and Y have been raised");
-end;
-
 function MetatableTests:TestDefensive()
     local defensive = Metatables.Defensive();
 
