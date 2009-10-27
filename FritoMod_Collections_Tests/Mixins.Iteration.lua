26dbe06bdf95db646c49d4f127f28a511127226e
Aaron Faanes <dafrito@gmail.com>
2009-10-27 08:19:23 -0500

    [new test] Mixable test suite for Iteration

--- FritoMod_Collections_Tests/Mixins.Iteration.lua
+++ FritoMod_Collections_Tests/Mixins.Iteration.lua
@@ -1,1 +1,75 @@
-local Suite = ReflectiveTestSuite:New("FritoMod_Collections.Mixins.Iteration");
+if Mixins == nil then
+    Mixins = {};
+end;
+
+function Mixins.IterationTests(Suite, library)
+    local function NewIterable()
+        local iterable = library.New();
+        library.Insert(iterable, "A");
+        library.Insert(iterable, "BB");
+        library.Insert(iterable, "CCC");
+        return iterable;
+    end;
+
+    function Check(iterable, key, value)
+        Assert.Equals(library.Get(iterable, key), value, "Key is in iterable: " .. tostring(key));
+        Assert.Equals(library.KeyFor(iterable, value), key, "Value is in iterable: " .. tostring(key));
+    end;
+
+    function Suite:TestBidiIterator()
+        local iterable = NewIterable();
+        local counter = Tests.Counter();
+        for key, value in library.BidiPairIterator(iterable) do
+            counter.Hit();
+            Check(iterable, key, value);
+        end;
+        counter.Assert(3);
+    end;
+
+    function Suite:TestBidiIteratorNext()
+        local iterable = NewIterable();
+        local iterator = library.BidiPairIterator(iterable);
+        local counter = Tests.Counter();
+        while true do
+            local key, value = iterator:Next();
+            if key == nil then
+                break;
+            end;
+            counter.Hit();
+            Check(iterable, key, value);
+        end;
+        counter.Assert(3);
+    end;
+
+    function Suite:TestBidiIteratorPrevious()
+        local iterable = NewIterable();
+        local counter = Tests.Counter();
+        local iterator = library.BidiPairIterator(iterable);
+        -- Seek the iterator to the end
+        while iterator() do end;
+        while true do
+            local key, value = iterator:Previous();
+            if key == nil then
+                break;
+            end;
+            counter.Hit();
+            Check(iterable, key, value);
+        end;
+        counter.Assert(3);
+    end;
+
+    function Suite:TestBidiIteratorStress()
+        local iterable = NewIterable();
+        local iterator = library.BidiPairIterator(iterable);
+        local stride = 0;
+        while stride <= library.Size(iterable) do
+            stride = stride + 1;
+            for i=1, stride do
+                Check(iterable, iterator:Next());
+            end;
+            for i=stride, 1, -1 do
+                Check(iterable, iterator:Previous());
+            end;
+        end;
+    end;
+end;
