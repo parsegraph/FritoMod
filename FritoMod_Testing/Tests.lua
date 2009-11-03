590cb9dadde22745a9113b131871034bc00917d6
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-03 16:59:31 -0600

    [new] Stack trace functions, sufficiently abstracting debugstack

--- FritoMod_Testing/Tests.lua
+++ FritoMod_Testing/Tests.lua
@@ -17,6 +17,126 @@ do
     end);
 end;
 
+-- Returns the full stack trace, or up to MAX_STACK_TRACE levels of stack trace information. Each
+-- stack level is represented by a table containing information provided by debug.getinfo.
+--
+-- returns
+--     a list of stack levels. Stack levels are in the format specified by debug.getinfo
+-- throws
+--     if debug.getinfo is not available
+local MAX_STACK_TRACE = 999;
+function Tests.FullStackTrace()
+    assert(debug, "FullStackTrace is not available without debug");
+    local stackTrace = {};
+	for i=1, MAX_STACK_TRACE do
+        local stackLevel = debug.getinfo(i);
+        if not stackLevel then
+            break;
+        end;
+        table.insert(stackTrace, stackLevel);
+    end;
+    return stackTrace;
+end;
+
+-- Returns a partial stack trace. It returns the head of the stack, and the tail
+-- of the stack. The amount of stack levels contained by either element is determined
+-- by numHead and numTail. You may also skip some of the head elements, since these
+-- are typically associated with debugging output and not relevance to the stack trace.
+--
+-- The returned stacks will never overlap; if skip + numHead + numTail > the number of stacks, then
+-- the head stack will contain the overlapping stacks. Otherwise, the middle stack levels are
+-- lost.
+--
+-- This function is an alias for debugstack, if that function is available.
+--
+-- skip:number
+--     the number of stack levels to skip. These levels are ignored, but are not subtracted from
+--     numHead. Defaults to 2.
+-- numHead:number
+--     the number of stack levels to contain in the head stack trace. If this is greater than the
+--     size of the stack, all stack levels are contained in that trace. The head stack trace is
+--     offset by skip, but the skipped levels are not subtracted from numHead. Defaults to 10.
+-- numTail:number
+--     the number of stack levels to contain in the tail stack trace. The head stack has 
+--     precedence over the tail stack; overlapping values will be added to the head stack and not
+--     the tail stack. If they are not overlapping, then the tail stack will contain numTail stack
+--     levels. Defaults to 10
+-- returns
+--     headStackTrace:table
+--         a list of stack levels representing the head of the stack, as described above. Each
+--         stack level is a table containing information. The level's information is provided by
+--         debug.getinfo
+--     tailStackTrace:table
+--         a list of stack levels representing the tail of the stack, as described above. Each
+--         stack level is a table containing information. The level's information is provided by
+--         debug.getinfo
+function Tests.PartialStackTrace(skip, numHead, numTail)
+    if debugstack then
+        return debugstack(skip, numHead, numTail);
+    end;
+	skip = math.max(skip, 2);
+	numHead = numHead or 10;
+	numTail = numTail or 10;
+    local stackTrace = Tests.FullStackTrace();
+    for i=1, skip do
+        table.remove(stackTrace, 1);
+    end;
+    local headStackTrace = {};
+    for i=1, math.min(#stackTrace, numHead) do
+        table.insert(headStackTrace, stackTrace[i]);
+	end;
+    local tailStackTrace = {};
+    for i=math.max(#stackTrace - numTail, numHead) + 1, #stackTrace do
+        table.insert(tailStackTrace, stackTrace[i]);
+	end;
+    return headStackTrace, tailStackTrace;
+end;
+
+-- Formats a stack level to be in the form returned by debugstack.
+--
+-- stackLevel:table
+--     a stack level, as returned by debug.getinfo
+-- returns
+--     a string describing the stack level, in the same format as returned by
+--     debugstack
+function Tests.FormatStackLevel(stackLevel)
+    if stackLevel.source:find("^[@=]") then
+        stackLevel.source = stackLevel.source:sub(2);
+    else
+        stackLevel.source = stackLevel.source:gsub("\n.*", "");
+        stackLevel.source = ("[string %s]"):format(stackLevel.source);
+    end
+    local stackLevelDescription = ("%s:%s: in %s `%s'"):format(
+        stackLevel.source,
+        stackLevel.currentline,
+        stackLevel.namewhat,
+        stackLevel.name or "(unnamed)"
+    );
+    return stackLevelDescription:gsub("/", "\\");
+end;
+
+-- Formats a stack trace, emulating the way debugstack formats it. As a result,
+-- this function is redundant if debugstack is available.
+-- 
+-- stackTrace:table
+--     a list of stack levels. Each level should be in the form as returned by debug.getinfo
+-- tailStackTrace:table
+--     optional. A list of stack levels, same format as above
+-- returns
+--     a string describing the stack trace
+function Tests.FormatStackTrace(stackTrace, tailStackTrace)
+    local stackString = "";
+    local function concat(stackLevel)
+        stackString = stackString .. Tests.FormatStackLevel(stackLevel) .. "\n";
+    end;
+    Lists.EachValue(stackTrace, concat);
+    if tailStackTrace then
+        stackString = stackString .. "...\n";
+        Lists.EachValue(tailStackTrace, concat);
+    end;
+    return stackString;
+end;
+
 function Tests.Choke(choke)
     local count = 0;
     return function()
