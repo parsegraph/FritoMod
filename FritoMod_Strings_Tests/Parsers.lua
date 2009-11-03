d8f41d90f9b0e0aec13bd95e645bf21fe1193e8a
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-03 12:08:57 -0600

    Strings_Tests project now uses require

--- FritoMod_Strings_Tests/Parsers.lua
+++ FritoMod_Strings_Tests/Parsers.lua
@@ -1,3 +1,11 @@
+if nil ~= require then
+    require "FritoMod_Testing/ReflectiveTestSuite";
+    require "FritoMod_Testing/Assert";
+    require "FritoMod_Testing/Tests";
+
+    require "FritoMod_Strings/Parsers";
+end;
+
 local ParsersTests = ReflectiveTestSuite:New("FritoMod_Strings.Parsers");
 
 function ParsersTests:TestParseTime()
