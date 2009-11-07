fe259200c955af3dd4d3821884ea9813d6543153
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-07 11:29:12 -0600

    [new] Added empty Collections.Functions library

---
+++ FritoMod_Collections/Functions.lua
@@ -0,0 +1,10 @@
+if nil ~= require then
+    require "FritoMod_Functional/currying";
+
+    require "FritoMod_Collections/Lists";
+end;
+
+if nil == Functions then
+    Functions = {};
+end;
+local Functions = Functions;
