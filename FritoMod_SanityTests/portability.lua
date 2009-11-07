2b56740ca92f5e4710fe1f7d73bec9f841671392
Aaron Faanes <dafrito@gmail.com>
2009-11-07 14:14:58 -0600

    [new test] Stack trace accessor test

--- FritoMod_SanityTests/portability.lua
+++ FritoMod_SanityTests/portability.lua
@@ -2,3 +2,15 @@
 -- indicates that the environment is lacking some required, albeit non-standard, components.
 
 local Suite = ReflectiveTestSuite:New("FritoMod_SanityTests.portability");
+
+function Suite:TestStackTraceAccessors()
+    if debugstack then
+        Assert.Type("function", debugstack, "debugstack is a function");
+        local stack = debugstack();
+        Assert.Type("string", stack, "debugstack returns a string");
+    else
+        Assert.Type("function", debug.getinfo, "debug.getinfo is a function");
+        local level = debug.getinfo(1);
+        Assert.Type("table", level, "getinfo returns a stack level");
+    end;
+end;
