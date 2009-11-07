f60b1d5fcb7eba765da5f9ce08838372601bf715
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-07 14:13:04 -0600

    [new test] Added empty portability, sanity test suites

---
+++ FritoMod_SanityTests/portability.lua
@@ -0,0 +1,4 @@
+-- Tests that assert that non-standard functionality is available to FritoMod. Test failure
+-- indicates that the environment is lacking some required, albeit non-standard, components.
+
+local Suite = ReflectiveTestSuite:New("FritoMod_SanityTests.portability");
---
+++ FritoMod_SanityTests/sanity.lua
@@ -0,0 +1,4 @@
+-- Tests that assert some non-intuitive or plain ambiguous behavior. These tests only assert
+-- lua-specific functionality, so test failures indicate an incompatible lua version.
+
+local Suite = ReflectiveTestSuite:New("FritoMod_SanityTests.sanity");
