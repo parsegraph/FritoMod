07f8424104d7039d8bcd86bba97ef63931492b7d
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-07 11:12:07 -0600

    [new test] New empty suite for Functional

---
+++ FritoMod_Functional_Tests/Functions.lua
@@ -0,0 +1,9 @@
+if nil ~= require then
+    require "FritoMod_Testing/ReflectiveTestSuite";
+    require "FritoMod_Testing/Assert";
+    require "FritoMod_Testing/Tests";
+
+    require "FritoMod_Functional/Functions";
+end;
+
+local Suite = ReflectiveTestSuite:New("FritoMod_Functional.Functions");
