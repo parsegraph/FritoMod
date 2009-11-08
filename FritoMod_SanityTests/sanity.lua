0e7538b6af630e9e91fea21ddd8c194f4db69b7a
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-08 00:52:04 -0600

    [new test] Tests proving that array length in lua is truly absurd.

--- FritoMod_SanityTests/sanity.lua
+++ FritoMod_SanityTests/sanity.lua
@@ -43,3 +43,58 @@ function Suite:TestUnpackWithNils()
     Assert.Equals(2, total, "Unpack unpacks correctly when array is of correct size");
     Assert.Equals(1, nonNils, "Unpack passes non-nil values");
 end;
+
+-- Demonstrates that intermediate nil values cause problems even if the array exists
+-- almost entirely as a non-nil structure. They even cause problems if the array has
+-- existed with only non-nil elements.
+--
+-- In other words, you simply never know what the length is if your array is sparse and
+-- you're performing insertions or deletions.
+function Suite:TestArrayLengthIsAbsurd()
+    local inconsistencies = {};
+    for insertions=1, 100 do
+        for removalIndex=1, insertions do
+            local list = {};
+            for i=1, insertions do
+                table.insert(list, true);
+            end;
+            list[removalIndex] = nil;
+            if removalIndex == insertions and #list ~= insertions - 1 then
+                table.insert(inconsistencies, {insertions, removalIndex, #list});
+            elseif #list ~= insertions then
+                table.insert(inconsistencies, {insertions, removalIndex, #list});
+            end;
+        end;
+    end;
+    assert(#inconsistencies > 0, "Array length is inconsistent");
+end;
+
+-- Demonstrates that even primed arrays still have inconsistencies with array length.
+-- While investigating supporting nil values in Curry, I found out that {nil, nil, nil, ...}
+-- seemed to preserve its length in most cases as long as the last element was not nil.
+-- This turned out to be false, as this test case demonstrates. Specifically, things
+-- start falling apart around 17 insertions, and never get any better. It essentially
+-- approaches the inconsistencies found in the previous test case.
+--
+-- It may be possible that supporting sub-16 nil arguments in currying is possible, but
+-- it seems to be about as error-prone as one can imagine to code, probably not anywhere
+-- near performant, will never handle trailing nil values properly, and is a very rare use-
+-- case anyway.
+function Suite:TestAbsurdArrayLengthWithPrimedLengthTable()
+    local inconsistencies = {};
+    -- Changing the max intended length to 16 or lower will yield a failing test, meaning
+    -- this strategy seems to work. Of course, this is only testing one nil value.
+    for intendedLength=1, 100 do
+        for removalIndex=1, intendedLength-1 do
+            -- Creates a list that seems to be primed such that its length is more consistent
+            -- than one created by table.insert alone.
+            local code = ("return { %s }"):format(("true, "):rep(intendedLength)); 
+            local list = assert(loadstring(code))();
+            list[removalIndex] = nil;
+            if #list ~= intendedLength then 
+                table.insert(inconsistencies, {insertions, removalIndex, #list});
+            end;
+        end;
+    end;
+    assert(#inconsistencies > 0, "Array length is still not consistent");
+end;
