6b7533a82d9162e29cb0e4e0973d7cedf39b2170
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-09 19:09:08 -0600

    [new, new test] Tests.Value

--- FritoMod_Testing_Tests/Tests.lua
+++ FritoMod_Testing_Tests/Tests.lua
@@ -6,6 +6,26 @@ end;
 
 local Suite = ReflectiveTestSuite:New("FritoMod_Testing.Tests");
 
+function Suite:TestValue()
+    local value = Tests.Value();
+    Assert.Nil(value.Get(), "Value is initially nil");
+    value.Set(true);
+    Assert.Equals(true, value.Get(), "Value can be set with Set, and retrieved with Get");
+    Assert.Exception("Value fails when asserted for unexpected value", value.Assert, false);
+    Assert.Success("Value succeeds when asserted for expected value", value.Assert, true);
+end;
+
+function Suite:TestValueAcceptsInitialValue()
+    local value = Tests.Value(true);
+    Assert.Equals(true, value.Get(), "Value accepts initial values");
+end;
+
+function Suite:TestValueAllowsMethodCalls()
+    local value = Tests.Value();
+    value:Set(true);
+    Assert.Equals(true, value:Get(), "Value allows method-style calls");
+end;
+
 function Suite:TestSimpleFlagMechanics()
     local flag = Tests.Flag();
     assert(not flag.IsSet(), "Flag starts unset");
