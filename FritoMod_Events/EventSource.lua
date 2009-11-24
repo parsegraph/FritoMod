4b76f5713e3ba227f2aea71f3532473a82f56cf2
Aaron Faanes <Aaron@fritocomp.(none)>
2009-11-24 09:48:16 -0600

    [see #29, new] EventSource:AddListener

--- FritoMod_Events/EventSource.lua
+++ FritoMod_Events/EventSource.lua
@@ -4,9 +4,30 @@ if nil ~= require then
 
     require "FritoMod_OOP/OOP-Class";
     
+    require "FritoMod_Collections/Lists";
     require "FritoMod_Collections/Tables";
 end;
 
 -- EventSource automatically manages the lifecycle of events and their registered listeners.
 EventSource = OOP.Class();
 local EventSource = EventSource;
+
+function EventSource:Constructor()
+    self.registry = {};
+    Metatables.ConstructedValue(self.registry, Tables.New);
+end;
+
+-- Registers the specified listener to receive events with the specified event name.
+--
+-- eventName:string
+--     the name of the event for which the listener will expect events
+-- listenerFunc, ...:function
+--     receives events for the specified event 
+-- returns:one-time function
+--     a function that removes the specified listener
+function EventSource:AddListener(eventName, listenerFunc, ...)
+    assert(type(eventName) == "string", "eventName must be a string");
+    listenerFunc = Curry(listenerFunc, ...);
+    local listeners = self.registry[eventName];
+    return Lists.Insert(listeners, listenerFunc);
+end;
--- FritoMod_Events_Tests/EventSource.lua
+++ FritoMod_Events_Tests/EventSource.lua
@@ -9,6 +9,9 @@ end;
 local Suite = ReflectiveTestSuite:New("FritoMod_Events.EventSource");
 
 function Suite:TestEventSource()
+    local value = Tests.Value();
     local source = EventSource:New();
+    local remover = source:AddListener("Foo", value.Set);
+    Assert.Type("function", remover, "AddListener returns a callable");
+    Assert.Exception("AddListener accepts only string event names", source.AddListener, source, 1, value.Set);
 end;
-
