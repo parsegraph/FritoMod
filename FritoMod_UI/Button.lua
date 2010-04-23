f6fe592c171242683d9e16ccb472eda0361d66d1
Aaron Faanes <Aaron@fritocomp.(none)>
2010-04-23 18:11:07 -0500

    [broken] Committed button as-is

--- FritoMod_UI/Button.lua
+++ FritoMod_UI/Button.lua
@@ -13,9 +13,32 @@ function Button:Constructor()
 	end;
 end;
 
+function Button:SetSpell(spell)
+	self.spell=spell;
+end;
+
+function Button:SetTexture(texture)
+	self.texture=texture;
+end;
+
 function Button:ConstructChildren()
-	self.frame = CreateFrame("Button");
-	self.frame:SetScript("OnClick", function()
+	local b = CreateFrame("Button",nil,nil,"SecureActionButtonTemplate");
+	b:HookScript("OnClick", function()
 		Lists.CallEach(self.listeners);
 	end);
+	self.frame=b;
+end;
+
+function Button:UpdateLayout()
+	local b=self.frame;
+	Button.super.UpdateLayout(self);
+	if self.texture then
+		b:SetNormalTexture(self.texture);
+		b:SetHighlightTexture("Interface/Buttons/ButtonHilight-Square");
+		b:SetPushedTexture("Interface/Buttons/UI-Quickslot-Depress");
+	end;
+	if self.spell then
+		b:SetAttribute("type", "macro");
+		b:SetAttribute("macrotext", "/cast " .. self.spell);
+	end;
 end;
