a531e6a368cbf7fad0544f8da2de7f385f8b6a25
Aaron Faanes <Aaron@fritocomp.(none)>
2010-04-24 11:57:31 -0500

    Added a very trivial Bar implementation

---
+++ FritoMod_UI/Bar.lua
@@ -0,0 +1,23 @@
+Bar = OOP.Class(DisplayObject);
+
+function Bar:ConstructChildren()
+	self.frame = CreateFrame("Frame");
+	self.group=self.frame:CreateAnimationGroup();
+	local scale=self.group:CreateAnimation("scale");
+	scale:SetOrigin("LEFT",0,0);
+	scale:SetDuration(3);
+	scale:SetScale(10,1);
+end;
+
+function Bar:SetTexture(texture)
+	self.texture=texture;
+	self:InvalidateLayout();
+end;
+
+function Bar:UpdateLayout()
+	Bar.super.UpdateLayout(self);
+	self.frame:SetBackdrop({
+		bgFile = self.texture
+	});
+	self.group:Play();
+end;
