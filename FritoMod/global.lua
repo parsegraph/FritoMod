ddc7b48ea97a8fcf667acc3d0823f70287ba8312
Aaron Faanes <dafrito@gmail.com>
2010-05-12 03:02:55 -0500

    global now replaces print

--- FritoMod/global.lua
+++ FritoMod/global.lua
@@ -1,3 +1,13 @@
 if nil ~= require then
 	require "FritoMod/CreateTestSuite";
 end;
+
+old_print=print;
+print=function(...)
+	if Strings then
+		Strings.__print=old_print;
+		Strings.Print(...);
+	else
+		old_print(...);
+	end;
+end;
