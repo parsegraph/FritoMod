fcefb513c73369c8afd7abdc6ec768ded062995b
Aaron Faanes <dafrito@gmail.com>
2010-05-14 07:45:12 -0500

    Fixed Slash to actually work with Blizzard's slash commands

--- FritoMod_Thonik/Slash.lua
+++ FritoMod_Thonik/Slash.lua
@@ -7,7 +7,7 @@ Slash={
 };
 
 function Slash.Run(cmd, ...)
-	local h=Slash.Registry[string.lower(cmd)];
+	local h=Slash.Registry[cmd];
 	if not h then
 		error("cmd must match a registered handler: "..cmd);
 	end;
@@ -22,28 +22,23 @@ function Slash.Register(name, handler, ...)
 		end;
 		return;
 	end;
-	Slash.Registry[string.lower(name)] = handler;
-	local remover=Functions.OnlyOnce(function()
-		Slash.Registry[string.lower(name)]=nil;
-	end);
+	Slash.Registry[name] = handler;
 	if SlashCmdList then
 		local upper=string.upper(name);
-		-- Set it to nil to force __newindex to fire on our new handler
-		SlashCmdList[upper]=nil;
-		SlashCmdList[upper]=handler;
-		i=1;
+		SlashCmdList[upper]=function(...)
+			-- This must be nested, since this value is saved and immutable
+			-- once it's first used.
+			Slash.Registry[name](...);
+		end;
+		local i=1;
 		while _G["SLASH_"..upper..i] do
 			i=i+1;
 		end;
-		_G["SLASH_"..upper.i]="/"..name;
-		return Functions.OnlyOnce(function()
-			_G["SLASH_"..upper.i]=nil;
-			SlashCmdList[upper]=nil;
-			remover();
-		end);
-	else
-		return remover;
+		_G["SLASH_"..upper..i]="/"..name;
 	end;
+	return Functions.OnlyOnce(function()
+		Slash.Registry[name]=nil;
+	end);
 end;
 
 function RegisterSlash(handler, ...)
