83019b6772d6ddc1b6e639476db0b3f526cbbf00
Aaron Faanes <dafrito@gmail.com>
2010-05-15 03:28:33 -0500

    [new] Merchant for autosell, autostock

---
+++ FritoMod_Thonik/Merchant.lua
@@ -0,0 +1,52 @@
+-- Automatic Item Selling
+if nil ~= require then
+	require "wowbench/BlizzardInterface/FrameXML/ContainerFrame";
+	
+	require "FritoMod_Functional/currying";
+	require "FritoMod_Events/Events";
+end;
+
+Merchant={
+	sellRules={},
+	stockRules={}
+};
+
+Merchant.SellRule=Curry(Lists.InsertFunction, Merchant.sellRules);
+Merchant.StockRule=Curry(Lists.InsertFunction, Merchant.stockRules);
+
+Events.MERCHANT_SHOW(function()
+    local total=0;
+    for bag=0,4 do
+        for slot=1,GetContainerNumSlots(bag) do
+			local sell;
+			for i=1,#Merchant.sellRules do
+				local r=Merchant.sellRules[i](bag, slot);
+				if r==true then
+					sell=true;
+				elseif r==false then
+					sell=false;
+					break;
+				end;
+			end;
+			if sell then
+				local q=select(2, GetContainerItemInfo(bag, slot));
+				local price=select(11, GetItemInfo(link));
+				total=total+(q*price);
+				UseContainerItem(bag, slot);
+				print('Selling:', GetContainerItemLink(bag, slot));
+			end;
+		end;
+	end;
+end);
+
+Events.MERCHANT_SHOW(function()
+    for i=1,GetMerchantNumItems() do
+		for j=1,#Merchant.stockRules do
+			local q=Merchant.stockRules[j](GetMerchantItemInfo(i));
+			local current=GetItemCount(GetMerchantItemLink(i));
+			if q >= current then
+				BuyMerchantItem(i, math.floor((q - current)/q));
+			end;
+		end;
+	end;
+end);
