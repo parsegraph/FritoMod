2946d6d6e1d9e9333afef084c7861a8ad77b2ce3
Aaron Faanes <dafrito@gmail.com>
2010-05-16 07:12:42 -0500

    Stubbed out some methods for events

---
+++ WoW_UI/Frame-Events.lua
@@ -0,0 +1,17 @@
+if nil ~= require then
+	require "WoW_UI/Frame";
+end;
+
+local Frame=WoW.Frame;
+
+function Frame:SetScript(event, handler)
+	
+end;
+
+function Frame:RegisterEvent(event)
+
+end;
+
+function Frame:UnregisterEvent(event)
+
+end;
