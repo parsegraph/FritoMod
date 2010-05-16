5f433c1feff856aa10b59f0426660fe3a4a121af
Aaron Faanes <dafrito@gmail.com>
2010-05-16 07:13:02 -0500

    Removed Event's wowbench dependency

---
+++ FritoMod_Events_Tests/Events.lua
@@ -0,0 +1,15 @@
+local Suite=CreateTestSuite("FritoMod_Events/Events");
+
+function Suite:TestEvents()
+	local f=Tests.Flag();
+	local r=Events.FOO(function(a)
+		Assert.Equals(true, a, "Events must pass arguments to listeners");
+		f.AssertUnset("Events calls listeners only once per each event");
+		f.Raise();
+	end);
+	Events._call("FOO", true);
+	f.Assert("Events calls listeners exactly once per event");
+	r();
+	-- Test to ensure the remover actually worked
+	Events._call("FOO", true);
+end;
