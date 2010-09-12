7ca35a232bcc4fc028c44bbc7f7549a64204f199
Aaron Faanes <dafrito@gmail.com>
2010-09-12 13:42:43 -0500

    Added explanation to how builders work

--- FritoMod_UI/Builders.lua
+++ FritoMod_UI/Builders.lua
@@ -2,7 +2,54 @@ if nil ~= require then
     require "WoW_UI/Frame-Layout";
 end;
 
-Builders = Builders or {};
+-- Builders are simple methods for adding functionality to another object.
+-- I chain them together as an array of headless methods and use Lists.Build
+-- to construct my value. Here's an example:
+--
+-- local b=Builders;
+-- local s={
+--     Headless(b.Colored, "red"),
+--     Headless(b.Square, 64),
+--     Headless(b.Centered)
+-- };
+-- Lists.Build(s, UIParent:CreateTexture());
+--
+-- I feel like this is pretty useful. We don't get in the way of the
+-- underlying API, since we never "own" the texture or wrap it in a
+-- closed abstraction. 
+--
+-- The closure calling convention might throw people off, and it's redundant
+-- information at any rate, so I made a HeadlessBuilders, which lets us do
+-- the above in a much cleaner way:
+--
+-- local b=HeadlessBuilders;
+-- local s={
+--     b.Colored("red"),
+--     b.Square(64),
+--     b.Centered()
+-- };
+-- Lists.Build(s, UIParent:CreateTexture());
+-- 
+-- HeadlessBuilders is just a view over Builders, so we're not locking anyone
+-- into one form or another.
+--
+-- I like that builders deal with very primitive objects. Builders can
+-- be called directly, and they're very easy to understand. 
+--
+-- I like the "function-as-an-adjective" naming convention, since it's
+-- easy to read. It also highlights that these methods are all related.
+--
+Builders=Builders or {};
+HeadlessBuilders=setmetatable({}, {
+    __newindex=function(self,k,v)
+        Builders[k]=v;
+    end,
+    __index=function(self,k)
+        return function(...)
+            return Headless(Builders[k], ...);
+        end
+    end
+});
 
 function Builders.Colored(f,r,g,b,a)
     f:SetTexture(r,g,b);
