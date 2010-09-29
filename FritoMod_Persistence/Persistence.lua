24536a810cfaca5bee6918b89ee215b7c8a4643a
Aaron Faanes <dafrito@gmail.com>
2010-09-29 18:23:08 -0500

    Added callback for persistence, for loading/unloading purposes

--- FritoMod_Persistence/Persistence.lua
+++ FritoMod_Persistence/Persistence.lua
@@ -1,6 +1,8 @@
 if nil~=require then
     require "FritoMod_Functional/currying";
     require "FritoMod_Functional/Functions";
+    require "FritoMod_Functional/Callbacks";
+    require "FritoMod_Collections/Lists";
     require "FritoMod_Events/Events";
 end;
 
@@ -15,21 +17,22 @@ Persistence=setmetatable({}, {
     end,
 
     __newindex=function(self, key, value)
+        -- TODO: This doesn't need to assert persistence; we could override these
+        -- values on load.
         AssertPersistence();
         _Persistence[key]=value;
     end
 });
 
-function Functions.Persistent(func, ...)
-    func=Curry(func, ...);
-    return function(...)
-        AssertPersistence();
-        return func(...);
-    end;
-end;
+local listeners={};
+local removers;
+Callbacks.Persistence=Curry(Lists.InsertFunction, listeners);
 
 Events.ADDON_LOADED(function(addon)
     if addon=="FritoMod_Persistence" then
         _Persistence=_Persistence or {};
     end;
+    removers=Lists.MapCall(listeners);
 end);
+
+Events.PLAYER_LOGOUT(Lists.CallEach, removers);
