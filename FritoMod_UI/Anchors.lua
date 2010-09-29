4a407d331469e670af3aade4739cedd76baebc3b
Aaron Faanes <dafrito@gmail.com>
2010-09-29 18:24:24 -0500

    Added first attempt at persistent anchors

--- FritoMod_UI/Anchors.lua
+++ FritoMod_UI/Anchors.lua
@@ -3,6 +3,11 @@
 -- (i.e., Flip, Stack, Center, etc. anchors) along with persistent anchors (once I figure out
 -- how these should work).
 
+if nil ~= require then
+    require "FritoMod_Functional/Callbacks";
+    require "FritoMod_Persistence/Persistence";
+end;
+
 Anchors={};
 
 local reverses={
@@ -38,4 +43,44 @@ function Anchors.Share(frame, ref, anchor, gap)
     gap=gap or 0;
     anchor=anchor:lower();
     frame:SetPoint(anchor, ref, anchor, touchGaps[anchor](gap));
+end;
+
+do
+    local anchors;
+    local function CreateAnchor(name)
+        local f=CreateFrame("Frame", nil, UIParent);
+        anchors[name]=f;
+        f:SetWidth(10);
+        f:SetHeight(10);
+        anchors[name]=f;
+        return f;
+    end;
+    local savedKey="FritoMod.Anchors";
+    Callbacks.Persistence(function()
+        anchors={};
+        if Persistent[savedKey] then
+            for name,location in pairs(Persistent[savedKey]) do
+                local f=CreateAnchor(name);
+                f:SetPoint(unpack(location));
+            end;
+        end;
+        return function()
+            Persistent[savedKey]={};
+            for name,f in pairs(anchors) do
+                local location={f:GetPoint(1)};
+                if location[2]==nil then
+                    location[2]="UIParent";
+                end;
+                Persistent[savedKey][name]=location;
+            end;
+        end;
+    end);
+    function Anchors.Named(name)
+        assert(anchors, "Anchors has not yet been loaded");
+        if not anchors[name] then
+            local f=CreateAnchor(name);
+            f:SetPoint("center");
+        end;
+        return anchors[name];
+    end;
 end;
