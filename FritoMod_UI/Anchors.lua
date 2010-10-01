392b06e3126c3892f70a9f865785ac14e367a409
Aaron Faanes <dafrito@gmail.com>
2010-09-30 23:55:15 -0500

    Lots and lots of work to get persistent anchors working

--- FritoMod_UI/Anchors.lua
+++ FritoMod_UI/Anchors.lua
@@ -51,41 +51,72 @@ function Anchors.Center(frame, ref, anchor)
 end;
 
 do
-    local anchors;
-    local function CreateAnchor(name)
-        local f=CreateFrame("Frame", nil, UIParent);
-        anchors[name]=f;
-        f:SetWidth(10);
-        f:SetHeight(10);
-        f:SetMovable(true);
-        local t=f:CreateTexture();
-        t:SetTexture(1,1,1);
-        t:SetAllPoints();
-        anchors[name]=f;
-        return f;
-    end;
-
+    local anchorFrame=CreateFrame("Frame", nil, UIParent);
+    anchorFrame:SetAllPoints();
+    anchorFrame:SetFrameStrata("HIGH");
     local function CheckLocation(location)
         if not location or type(location) ~= "table" then
             return false;
         end;
-        if type(location.x) ~= "number" or type(location.y) ~= "number" then
+        if location.x==nil or location.y==nil then
             return false;
         end;
-        if type(location.point) ~= "string" then
+        if type(location.anchor) ~= "string" then
             return false;
         end;
         return true;
     end;
-    local savedKey="FritoMod.Anchors";
 
+    local function UnlockAnchor(anchor)
+        anchor:Show();
+        anchor:EnableMouse(true);
+        anchor:RegisterForDrag("LeftButton");
+        anchor:SetScript("OnDragStart", anchor.StartMoving);
+        anchor:SetScript("OnDragStop", anchor.StopMovingOrSizing);
+    end;
+
+    local function LockAnchor(anchor)
+        anchor:EnableMouse(false);
+        anchor:RegisterForDrag();
+        anchor:StopMovingOrSizing();
+        anchor:SetScript("OnDragStart", nil);
+        anchor:SetScript("OnDragStop", nil);
+        anchor:Hide();
+    end;
+
+    local locked=true;
+    local anchors;
+    local function CreateAnchor(name)
+        local f=CreateFrame("Frame", nil, anchorFrame);
+        anchors[name]=f;
+        f:SetWidth(10);
+        f:SetHeight(10);
+        f:SetMovable(true);
+        local white=f:CreateTexture();
+        white:SetTexture(1,1,1);
+        white:SetPoint("center");
+        white:SetWidth(7);
+        white:SetHeight(7);
+        local black=f:CreateTexture();
+        black:SetTexture(0,0,0);
+        black:SetAllPoints();
+        anchors[name]=f;
+        if not locked then
+            UnlockAnchor(f);
+        else
+            f:Hide();
+        end;
+        return f;
+    end;
+
+    local savedKey="FritoMod.Anchors";
     function Anchors.Load()
         anchors={};
         if Persistence[savedKey] then
             for name,location in pairs(Persistence[savedKey]) do
                 local f=CreateAnchor(name);
                 if CheckLocation(location) then
-                    f:SetPoint(location.anchor, UIParent, location.anchor, location.x, location.y);
+                    f:SetPoint(location.anchor, anchorFrame, location.anchor, location.x, location.y);
                 end;
             end;
         end;
@@ -118,24 +149,34 @@ do
         return anchors[name];
     end;
 
+    function Anchors.Lock()
+        if not locked then
+            locked=true;
+            for k,anchor in pairs(anchors) do
+                LockAnchor(anchor);
+            end;
+        end;
+        return Anchors.Unlock;
+    end;
+
     function Anchors.Unlock()
-        for k,anchor in pairs(anchors) do
-            anchor:RegisterForDrag("LeftButton");
-            anchor:SetScript("OnDragStart", anchor.StartMoving);
-            anchor:SetScript("OnDragStop", anchor.StopMovingOrSizing);
+        if locked then
+            locked=false;
+            for k,anchor in pairs(anchors) do
+                UnlockAnchor(anchor);
+            end;
         end;
         return Anchors.Lock;
     end;
 
-
-    function Anchors.Lock()
-        for k,anchor in pairs(anchors) do
-            anchor:RegisterForDrag();
-            anchor:StopMovingOrSizing();
-            anchor:SetScript("OnDragStart", nil);
-            anchor:SetScript("OnDragStop", nil);
-            anchor:Hide();
+    function Anchors.Toggle()
+        if locked then
+            Anchors.Unlock();
+        else
+            Anchors.Lock();
         end;
-        return Anchors.Unlock;
     end;
+    Anchors.ToggleLock=Anchors.Toggle;
+    Anchors.ToggleLocks=Anchors.Toggle;
+
 end;
