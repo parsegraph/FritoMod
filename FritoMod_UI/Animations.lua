1b69c8dd551fff6b9713e4f079969e693408b43b
Aaron Faanes <dafrito@gmail.com>
2010-10-15 12:39:49 -0500

    Added docs for Animations

--- FritoMod_UI/Animations.lua
+++ FritoMod_UI/Animations.lua
@@ -3,6 +3,53 @@ if nil~=require then
    require "WoW_UI/Frame-Animation";
 end
 
+-- Animations contains functions for creating different kinds of animations. I personally
+-- like Blizzard's API for animating, since I feel like it is well-designed. My only
+-- complaint is that it is somewhat verbose. These functions remove a lot of that verbosity.
+--
+-- We don't cover up Blizzard's functions, and we operate on Blizzard's animation objects
+-- directly, so you're free to mix and match this code and the official API.
+--
+-- -- We don't have helper functions for CreateFrame, since we haven't needed any.
+-- local f=CreateFrame("Frame", nil, UIParent);
+--
+-- -- I like using my helper functions since I find them quick to write and easy to understand
+-- -- but you're free to use Blizzard's stuff here.
+-- Frames.Size(f, 200, 20);
+-- Frames.Color(f, "red", .8);
+-- f:SetPoint("center");
+--
+-- -- Once again, I don't like covering up constructors unless I have a really good reason. I
+-- -- feel like having these exposed gives an assurance that there's no magic behind the scenes:
+-- -- really are just plain ol' objects.
+-- local ag=f:CreateAnimationGroup();
+--
+-- -- This creates an opacity change, all the way to transparent, in five seconds. All animation
+-- -- constructors follow this pattern:
+-- --
+-- -- Animations.[Alpha|Move|Rotate|Scale](ag, duration, ...)
+-- --
+-- -- I find this convention to be convenient when trying to remember the signatures.
+-- --
+-- -- This returns both the animation and the animation group. We don't need either, so we ignore
+-- -- them.
+-- Animations.Alpha(ag, 5, -1);
+--
+-- -- Here, we create a scale animation as well. It's also a 5-second duration scale. The 0,1 are
+-- -- multipliers we use for the scale. The 0 will reduce our frame to nothing on the horizontal axis.
+-- -- The 1 will do nothing for our vertical axis. This will show our frame shrinking to the middle,
+-- -- but remaining tall.
+-- local scale=Animations.Scale(ag, 5, 0, 1);
+--
+-- -- We also can set the origin of the scale. This makes our frame shrink from right to left, with the
+-- -- left side not moving. Blizzard provides a function to do this, but I like ours since we don't
+-- -- have to always specify the offsets.
+-- Animations.Origin(scale, "left");
+--
+-- -- Finally, play our animation. Once the animation is complete, it will reset to its original shape.
+-- -- You can tweak this by setting the looping behavior, or by adding a handler for the OnFinished 
+-- -- event.
+-- ag:Play();
 Animations={};
 
 local function GetGroup(agOrFrame)
@@ -12,6 +59,8 @@ local function GetGroup(agOrFrame)
    return agOrFrame;
 end;
 
+-- This function lets us define offsets using one number. It also lets
+-- us omit offsets - this is something that Blizzard's animation can't do.
 function Animations.Origin(animation, anchor, xOffset, yOffset)
     if xOffset==nil then
         xOffset=0;
