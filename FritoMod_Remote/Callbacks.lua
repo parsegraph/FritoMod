35fe7c6e138d6800c7342dabb929c0a3b918fb4a
Aaron Faanes <dafrito@gmail.com>
2010-10-22 19:09:51 -0500

    Added Callbacks, the receiving side of our string-chunk protocol

---
+++ FritoMod_Remote/Callbacks.lua
@@ -0,0 +1,33 @@
+if nil ~= require then
+    require "FritoMod_Functional/currying";
+end;
+
+Callbacks=Callbacks or {};
+
+local DELIMITER_BYTE=(":"):byte(1);
+
+function Callbacks.StringChunks(source, callback, ...)
+    callback=Curry(callback, ...);
+    local messages={};
+    return source(function(chunk, who, ...)
+        if chunk:byte(1)==DELIMITER_BYTE then
+            -- It's a headless chunk, so dispatch it directly.
+            callback(chunk:sub(2), who, ...);
+        else    
+            local header, data=unpack(Strings.Split(":", chunk, 2));
+            local id=who..header;
+            if not messages[id] then
+                messages[id]={};
+                messages[id].dataLength=#data;
+            end;
+            if data then
+                table.insert(messages[id], data);
+            end;
+            if not data or #data < messages[id].dataLength then
+                local message=table.concat(messages[id], "");
+                messages[id]=nil;
+                callback(message, who, ...);
+            end;
+        end;
+    end);
+end;
---
+++ FritoMod_Remote_Tests/Callbacks.lua
@@ -0,0 +1,30 @@
+local Suite=CreateTestSuite("FritoMod_Remote/Callbacks");
+
+function Suite:TestChunkStringCallback()
+    local callback=Objects.Value();
+    local flag=Tests.Flag();
+    local remover=Callbacks.StringChunks(callback.Set, function(message, who, ...)
+        flag.AssertUnraised();
+        Assert.Equals("Foo", message);
+        Assert.Equals("Frito", who);
+        flag.Raise();
+    end);
+    callback.Get()(":Foo", "Frito");
+    flag.Assert();
+end;
+
+function Suite:TestCallbackWithABrokenString()
+    local callback=Objects.Value();
+    local flag=Tests.Flag();
+    local remover=Callbacks.StringChunks(callback.Set, function(message, who, ...)
+        flag.AssertUnraised();
+        Assert.Equals("Fooderbug", message);
+        Assert.Equals("Frito", who);
+        flag.Raise();
+    end);
+    callback.Get()("1:Foo", "Frito");
+    callback.Get()("1:der", "Frito");
+    callback.Get()("1:bug", "Frito");
+    callback.Get()("1:", "Frito");
+    flag.Assert();
+end;
