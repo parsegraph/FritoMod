1c5e6dae24d7921d57dbe7e742a6f4ad5fddfc0f
Aaron Faanes <dafrito@gmail.com>
2010-10-26 19:20:59 -0500

    string<-->data parser

--- FritoMod_Serializers/Serializers-Data.lua
+++ FritoMod_Serializers/Serializers-Data.lua
@@ -1,38 +1,140 @@
+if nil ~= require then
+    require "FritoMod_Functional/Metatables";
+    require "FritoMod_Cursors/Cursors-Iterable";
+end;
+
 Serializers=Serializers or {};
+local Read, Write;
+
+local writers=Metatables.Defensive({
+    boolean=function(b)
+        if b==true then
+            return "B";
+        end;
+        return "b";
+    end,
+
+    string=function(s)
+        if s=='' then
+            return "S";
+        end;
+        return ("s%d%s"):format(#s, s);
+    end,
+
+    number=function(n)
+        if n % 1 == 0 then
+            return ("n%d"):format(n);
+        end;
+        return ("n%f"):format(n);
+    end,
+
+    ["function"]=function(f)
+        return Write(f());
+    end,
+
+    ["nil"]=function()
+        return "_";
+    end,
+
+    table=function(t)
+        local out="t";
+        for k,v in pairs(t) do
+            out=out..("k%s%s"):format(
+                Write(k),
+                Write(v)
+            );
+        end;
+        return out;
+    end
+});
+
+Write=function(v)
+    return writers[type(v)](v);
+end;
 
 function Serializers.WriteData(...)
     local out="";
     for i=1,select("#", ...) do
         local v=select(i, ...);
-        local m;
-        if v==true then
-            m="B";
-        elseif v==false then
-            m="b";
-        else
-            error("Unsupported "..type(v).." value: "..tostring(v));
-        end;
-        out=out..m;
+        out=out..Write(v);
     end;
     return out;
 end;
 
-function Serializers.ReadData(message)
+local readers=Metatables.Defensive();
+
+function readers.B(c)
+    return c:Get()=="B";
+end;
+readers.b=readers.B;
+
+function readers.s(c)
+    c:MarkNext(); -- Skip the 's'
+    c:PeekWhile(Strings.IsNumber);
+    local len=tonumber(c:MarkSnippet());
+    c:MarkNext(); 
+    c:Move(math.max(0, len-1));
+    return c:MarkSnippet();
+end;
+
+function readers.S(c)
+    return '';
+end;
+
+function readers._(c)
+    return nil;
+end;
+
+function readers.n(c)
+    c:Next(); -- Skip the 'n'
+    local number=c:Iterable():match("-?%d+%.?%d*", c:Index());
+    c:Move(#number-1);
+    return tonumber(number);
+end;
+
+function readers.t(c)
+    c:Next() -- Skip the 't'
+    local t={};
+    while c:Get()=="k" do
+        c:Next();
+        local k=Read(c);
+        c:Next();
+        local v=Read(c);
+        t[k]=v;
+    end;
+    return t;
+end;
+
+-- Version. We allow this to be anywhere in the string, but we 
+-- usually just ignore it.
+function readers.v(c)
+    c:Next() -- Skip the 'v'
+    c:PeekWhile(Strings.IsNumber);
+    return Read(c);
+end;
+
+function readers.l(c)
+    c:MarkNext(); -- Skip the 'l'
+    local t={};
+    c:PeekWhile(Strings.IsNumber);
+    local len=tonumber(c:MarkSnippet());
+    for i=1,len do
+        table.insert(t, Read(c));
+    end;
+    return t;
+end;
+
+Read=function(c)
+    return readers[c:Get()](c)
+end;
+
+function Serializers.ReadData(c)
+    if not OOP.InstanceOf(Cursors.Iterable, c) then
+        c=Cursors.Iterable:New(c);
+    end;
     local data={};
-    local dataCount=0;
-    local i=Strings.BidiIterator(message);
-    while i.Next() do
-        local p=i.Value();
-        local v;
-        if p=="B" then
-            v=true;
-        elseif p=="b" then
-            v=false;
-        else
-            error("Unsupported "..type(v).." value: "..tostring(v));
-        end;
-        dataCount=dataCount+1;
-        data[dataCount]=v;
+    while c:Next() do
+        table.insert(data, Read(c));
     end;
     return unpack(data);
 end;
--- FritoMod_Serializers_Tests/Serializers-Data.lua
+++ FritoMod_Serializers_Tests/Serializers-Data.lua
@@ -4,6 +4,58 @@ local function Check(...)
     Assert.Equals({...}, {Serializers.ReadData(Serializers.WriteData(...))});
 end
 
-function Suite:TestSerializeABoolean()
-    Check(true, false);
+function Suite:TestWriteAString()
+    Assert.Equals("s7No time", Serializers.WriteData("No time"));
 end;
+
+function Suite:TestWriteANumber()
+    Assert.Equals(("n%f"):format(3.14), Serializers.WriteData(3.14));
+end;
+
+function Suite:TestWriteABoolean()
+    Assert.Equals("BbB", Serializers.WriteData(true,false,true));
+end;
+
+function Suite:TestWriteATable()
+    Assert.Equals("tks2nos4time", Serializers.WriteData({
+        no="time",
+    }));
+end;
+
+function Suite:TestWriteATableWithANestedTable()
+    Assert.Equals("tks6nestedtks4deeptks6answern42", Serializers.WriteData({
+        nested={
+            deep={
+                answer=42
+            }
+        }
+    }));
+end;
+
+function Suite:TestReadAString()
+    Assert.Equals("No time", Serializers.ReadData("s7No time"));
+end;
+
+function Suite:TestReadANumber()
+    Assert.Equals(3.14, Serializers.ReadData(("n%f"):format(3.14)));
+end;
+
+function Suite:TestReadABoolean()
+    Assert.Equals(true, Serializers.ReadData("B"));
+    Assert.Equals(false, Serializers.ReadData("b"));
+end;
+
+function Suite:TestReadATable()
+    Assert.Equals({no="time"}, Serializers.ReadData("tks2nos4time"));
+end;
+
+function Suite:TestReadANestedTable()
+    Assert.Equals({
+        nested={
+            deep={
+                answer=42
+            }
+        }
+    }, Serializers.ReadData("tks6nestedtks4deeptks6answern42"));
+end;
+
