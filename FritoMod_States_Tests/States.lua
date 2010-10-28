e376c188b299d4c83b261bf25ed2a7d490c7cc8a
Aaron Faanes <dafrito@gmail.com>
2010-10-28 18:40:34 -0500

    Removed our StateMachine stuff
    It's just not maintained, not used, and makes for really ugly code.

--- FritoMod_States_Tests/States.lua
+++
@@ -1,9 +0,0 @@
-if nil ~= require then
-    require "FritoMod_Testing/ReflectiveTestSuite";
-    require "FritoMod_Testing/Assert";
-    require "FritoMod_Testing/Tests";
-
-    require "FritoMod_States/States";
-end;
-
-local StatesTests = ReflectiveTestSuite:New("FritoMod_States.States");
