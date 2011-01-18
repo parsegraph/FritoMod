5120da43bb4a8c39dfef630799f4ddda5bd067fe
Aaron Faanes <dafrito@gmail.com>
2011-01-18 06:34:01 -0600

    Implemented frame:Show and frame:Hide

--- wow/Frame-Layout.lua
+++ wow/Frame-Layout.lua
@@ -1,8 +1,13 @@
 if nil ~= require then
 	require "wow/Frame";
+	require "wow/Frame-Events";
 end;
 
 local Frame=WoW.Frame;
+
+Frame:AddConstructor(function(self)
+    self.shown=true;
+end);
 
 function Frame:SetAllPoints()
 
@@ -33,11 +38,21 @@ function Frame:GetWidth()
 end;
 
 function Frame:Show()
-
+    if not self.shown then
+        self.shown=true;
+        self:FireEvent("OnShow");
+    end;
 end;
 
 function Frame:Hide()
+    if self.shown then
+        self.shown=false;
+        self:FireEvent("OnHide");
+    end;
+end;
 
+function Frame:IsShown()
+    return self.shown;
 end;
 
 function Frame:SetAlpha()
