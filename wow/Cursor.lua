84ea9ef8e81c4478a2ecf1b68c422060401c6998
Aaron Faanes <dafrito@gmail.com>
2011-01-18 03:01:38 -0600

    [new] wow/Cursor, to fake various kinds of mouse events

---
+++ wow/Cursor.lua
@@ -0,0 +1,50 @@
+if nil ~= require then
+    require "Functions";
+    require "OOP-Class"
+end;
+
+WoW=WoW or {};
+
+WoW.Cursor=OOP.Class();
+local Cursor=WoW.Cursor;
+
+function Cursor:Constructor(world)
+    self.world=world;
+end;
+
+function Cursor:GetWorld()
+    return self.world;
+end;
+
+function Cursor:AssertWorld(frame)
+    assert(frame.GetWorld, "Frame must be created by FritoMod; real frames aren't supported");
+    assert(frame:GetWorld()==self:GetWorld(), "Frame must be of the same world");
+end;
+
+function Cursor:Enter(frame)
+    if self.hoveredFrame == frame then
+        return Noop;
+    end;
+    if self.hoveredFrame then
+        self:Leave();
+    end;
+    self:AssertWorld(frame);
+    self.hoveredFrame=frame;
+    self.hoveredFrame:FireEvent("OnEnter");
+    return Functions.OnlyOnce(self, "Leave", frame);
+end;
+
+function Cursor:Click(frame)
+    self:AssertWorld(frame);
+    self:Hover(frame);
+    self.hoveredFrame:FireEvent("OnClick");
+end;
+
+function Cursor:Leave()
+    if not self.hoveredFrame then
+        return;
+    end;
+    local oldFrame=self.hoveredFrame;
+    self.hoveredFrame=nil;
+    oldFrame:FireEvent("OnLeave");
+end;
---
+++ wow/tests/Cursor.lua
@@ -0,0 +1,18 @@
+if nil ~= require then
+    require "wow/Frame-Events";
+    require "wow/World";
+end;
+local Suite=UnitTest("wow.Cursor");
+
+function Suite:TestCursor()
+    local world=WoW.World:New();
+    local frame=WoW.Frame:New(world);
+
+    local flag=Tests.Flag();
+    frame:SetScript("OnEnter", flag.Raise);
+    frame:SetScript("OnLeave", flag.Lower);
+    local r=world:GetCursor():Enter(frame);
+    flag.Assert()
+    r();
+    flag.AssertUnset()
+end;
