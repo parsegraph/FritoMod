be9a8921d93e926dbf9e570d67955e2a2ed8e044
Aaron Faanes <dafrito@gmail.com>
2011-12-26 04:34:37 -0600

    Added Callbacks.ImmediateQuantity

--- fritomod/Callbacks-Inventory.lua
+++ fritomod/Callbacks-Inventory.lua
@@ -1,3 +1,13 @@
+-- Callbacks for your inventory
+--[[
+
+Callbacks.Quantity("all", "Embersilk Cloth", printf, "You now have %d cloth!");
+
+--]]
+--
+-- See Also:
+-- Containers.SumItem
+
 if nil ~= require then
     require "fritomod/Lists";
     require "fritomod/Events";
@@ -6,6 +16,10 @@ end;
 
 Callbacks=Callbacks or {};
 
+-- Fires on inventory changes.
+--
+-- This only fires for changes to your inventory, so it will not fire
+-- on first-use. Use Callbacks.ImmediateQuantity.
 function Callbacks.Quantity(bag, item, func, ...)
     func=Curry(func, ...);
     bag=Containers.InterpretBagName(bag);
@@ -20,4 +34,10 @@ function Callbacks.Quantity(bag, item, func, ...)
             lastQuantity=newQuantity;
         end;
     end);
+end;
+
+function Callbacks.ImmediateQuantity(bag, item, func, ...)
+	func=Curry(func, ...);
+	func(Containers.SumItem(bag, item));
+	return Callbacks.Quantity(bag, item, func);
 end;
