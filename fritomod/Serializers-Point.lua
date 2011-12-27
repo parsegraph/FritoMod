5e0ce0c34bb4490ac27964b62bb463e2c2b746fd
Aaron Faanes <dafrito@gmail.com>
2011-12-27 11:08:47 -0600

    Added support for anchorTo in Serializers.SavePoint
    I don't know why I didn't support this before.

--- fritomod/Serializers-Point.lua
+++ fritomod/Serializers-Point.lua
@@ -9,6 +9,10 @@ function Serializers.VerifyPoint(location)
     assert(type(location) == "table", "location must be a table. Type: "..type(location));
     assert(type(location.anchor)=="string",
         "location.anchor must be a string, but it was: "..tostring(location.anchor));
+    if location.anchorTo then
+        assert(type(location.anchorTo)=="string",
+            "location.anchorTo must be a string, but it was: "..type(location.anchorTo));
+    end;
     assert(tonumber(location.x) or location.x==nil,
         "location.x looks invalid (not a number or nil): "..tostring(location.x));
     assert(tonumber(location.y) or location.y==nil,
@@ -17,13 +21,14 @@ end;
 
 function Serializers.LoadPoint(location, frame)
     Serializers.VerifyPoint(location);
-    frame:SetPoint(location.anchor, nil, location.anchor, location.x, location.y);
+    frame:SetPoint(location.anchor, nil, location.anchorTo, location.x, location.y);
 end;
 
 function Serializers.SavePoint(frame, pointNum)
     local location={};
-    local anchor,_,_,x,y=frame:GetPoint(pointNum);
+    local anchor,_,anchorTo,x,y=frame:GetPoint(pointNum);
     location.anchor=anchor;
+    location.anchorTo=anchorTo;
     location.x=x;
     location.y=y;
     Serializers.VerifyPoint(location);
