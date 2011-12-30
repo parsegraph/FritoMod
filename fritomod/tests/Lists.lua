282fc467634d5c76882520b9601ba21ca3b7af9f
Aaron Faanes <dafrito@gmail.com>
2011-12-30 06:18:23 -0600

    Removed unnecessary test from Lists; duplicated in iteration.lua

--- fritomod/tests/Lists.lua
+++ fritomod/tests/Lists.lua
@@ -10,11 +10,6 @@ function Suite:Array(...)
 	return {...};
 end;
 
-function Suite:TestListsContainsValue()
-	local buttons={"LeftButton1", "RightButton"};
-	assert(Lists.ContainsValue(buttons, "LeftButton", Strings.StartsWith));
-end;
-
 Mixins.ComparableIterationTests(Suite, Lists);
 Mixins.ArrayTests(Suite, Lists);
 Mixins.MutableArrayTests(Suite, Lists);
