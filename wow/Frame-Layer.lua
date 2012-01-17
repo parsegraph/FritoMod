785d1b56cd44355416b10a2370997f9b179db055
Aaron Faanes <dafrito@gmail.com>
2012-01-16 22:34:36 -0600

    Added stubs for draw layers

---
+++ wow/Frame-Layer.lua
@@ -0,0 +1,12 @@
+if nil ~= require then
+	require "fritomod/OOP-Class";
+	require "wow/Frame";
+end;
+
+local Frame = WoW.Frame;
+
+function Frame:SetDrawLayer()
+end;
+
+function Frame:GetDrawLayer()
+end;
