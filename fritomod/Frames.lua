4ed1c66fe08a6a5b13d85b35ba36b2a2b6fb0da4
Aaron Faanes <dafrito@gmail.com>
2012-01-16 22:34:06 -0600

    Frames.Insets fails on nil frame

--- fritomod/Frames.lua
+++ fritomod/Frames.lua
@@ -230,13 +230,11 @@ local INSETS_ZERO={
 	right=0
 };
 function Frames.Insets(f)
-	if f then
-		f=Frames.AsRegion(f);
-		if f and f.GetBackdrop then
-			local b=f:GetBackdrop();
-			if b then
-				return b.insets;
-			end;
+	f=Frames.AsRegion(f);
+	if f and f.GetBackdrop then
+		local b=f:GetBackdrop();
+		if b then
+			return b.insets;
 		end;
 	end;
 	return INSETS_ZERO;
