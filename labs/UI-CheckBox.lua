7efe77eba292611ac20966b804d5335da6a97a99
Aaron Faanes <dafrito@gmail.com>
2012-06-06 02:26:26 -0500

    First stab at a UI-CheckBox

---
+++ labs/UI-CheckBox.lua
@@ -0,0 +1,160 @@
+if nil ~= require then
+	require "wow/FontString";
+	require "wow/Frame-Container";
+	require "fritomod/Frames";
+	require "fritomod/UI-Icon";
+	require "fritomod/OOP-Class";
+	require "fritomod/Metatables-StyleClient";
+end;
+
+UI = UI or {};
+
+local CheckBox = OOP.Class(UI.Icon);
+UI.CheckBox = CheckBox;
+UI.Checkbox = UI.CheckBox;
+
+local DEFAULT_STYLE = {
+
+	-- Height of the checkbox and the text
+	height = 16,
+
+	-- Gap between the checkbox and the text
+	gap = 4,
+
+	-- Font for checkbox text
+	font = "default",
+
+	-- Explicit font size. Optional.
+	fontSize = nil,
+	
+	-- Where the icon is positioned relative to the text.
+	-- Either "LEFT" or "RIGHT"
+	iconPosition = nil,
+};
+
+function CheckBox:Constructor(parent, style, text)
+	parent = Frames.AsRegion(parent);
+	assert(Frames.IsFrame(parent), "Parent frame must be provided");
+	assert(parent.CreateTexture, "Provided parent must be a real frame");
+	assert(parent.CreateFontString, "Provided parent must be a real frame");
+
+	self.style = Metatables.StyleClient(style);
+	self.style:Inherits(DEFAULT_STYLE);
+
+	if not self.style.size then
+		self.style.size = self.style.height;
+	end;
+
+	self.style.backdrop = "none";
+
+	-- This should ideally use something like PrefixedStyleClient
+	local iconStyle = Metatables.StyleClient({}, self.style);
+	iconStyle.blendMode = "ADD";
+	iconStyle.drawLayer = "ADD";
+
+	self.icon = UI.Icon:New(parent, self.style);
+	self.icon:Set("checkbox");
+
+	self.mark = Frames.AsRegion(self.icon):CreateTexture(nil, "OVERLAY");
+	Frames.Texture(self.mark, "checkmark");
+	Anchors.ShareAll(self.mark, self.icon);
+	self.mark:Hide();
+
+	self.highlight = Frames.AsRegion(self.icon):CreateTexture(nil, "OVERLAY");
+	self.highlight:SetBlendMode("ADD");
+	Frames.Texture(self.highlight, "check highlight");
+	Anchors.ShareAll(self.highlight, self.icon);
+	self.highlight:Hide();
+
+	if not self.style.fontSize then
+		self.style.fontSize = self.style.height;
+	end;
+
+	if self.style.iconPosition then
+		self.style.iconPosition = self.style.iconPosition:upper();
+	else
+		self.style.iconPosition = "LEFT"
+	end;
+	assert(
+		   self.style.iconPosition == "LEFT"
+		or self.style.iconPosition == "RIGHT",
+		"Invalid icon position: " .. self.style.iconPosition);
+
+	self.text = Frames.Text(parent, self.style.font, self.style.height);
+	self.text:SetHeight(self.style.height);
+
+	self.listeners = ListenerList:New();
+
+	self.callbacks = {
+		Callbacks.Click(self.icon, self, "Toggle"),
+		Callbacks.EnterFrame(self.icon, Frames.Show, self.highlight)
+	};
+
+	self.listeners:Add(function(self, value)
+		if value then
+			self.mark:Show();
+		else
+			self.mark:Hide();
+		end;
+	end, self);
+
+	self.state = false;
+end;
+
+function CheckBox:OnChange(listener, ...)
+	return self.listeners:Add(listener, ...);
+end;
+
+function CheckBox:Set(value)
+	value = Bool(value);
+	if value == self.state then
+		return;
+	end;
+
+	self.state = value;
+	self.listeners:Fire(self.state);
+end;
+
+function CheckBox:IsChecked()
+	return Bool(self.state);
+end;
+CheckBox.IsSet=CheckBox.IsChecked;
+
+function CheckBox:Toggle()
+	self:Set(not self:IsChecked());
+end;
+
+function CheckBox:SetText(text)
+	self.text:SetText(text);
+end;
+
+function CheckBox:Anchor(anchor)
+	trace("Anchoring checkbox to " ..anchor);
+	Anchors.Clear(self.icon, self.text);
+	local first, second = self.icon, self.text;
+	if self.iconPosition == "RIGHT" then
+		first, second = self.text, self.icon;
+	end;
+	return Anchors.HJustifyFrom(anchor, self.style.gap, first, second);
+end;
+
+function CheckBox:Bounds(anchor)
+	local hcomp = Frames.HorizontalComponent(anchor);
+	-- Return the text only if it's really necessary. By default, we
+	-- prefer returning the icon.
+	if     self.style.iconPosition == "LEFT"  and hcomp == "RIGHT"
+		or self.style.iconPosition == "RIGHT" and hcomp == "LEFT" then
+		return self.text;
+	else
+		return self.icon;
+	end;
+end;
+
+function CheckBox:Destroy()
+	trace("Destroying check box");
+	if self.callbacks then
+		Lists.CallEach(self.callbacks);
+		self.callbacks = nil;
+	end;
+	Frames.Destroy(self.icon, self.text);
+end;
