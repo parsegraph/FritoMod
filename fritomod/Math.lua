266590b133081be3756e2a6e8751112f9d2bd7cc
Aaron Faanes <dafrito@gmail.com>
2012-06-23 18:08:05 -0500

    Added several Math methods and tests

--- fritomod/Math.lua
+++ fritomod/Math.lua
@@ -4,6 +4,46 @@ end;
 
 Math = Math or {};
 
+local function CheckRange(min, value, max)
+	assert(type(min) == "number", "min must be a number");
+	assert(type(value) == "number", "value must be a number");
+	assert(type(max) == "number", "value must be a number");
+	assert(min <= max, "min must be less than max");
+end;
+
+function Math.Clamp(min, value, max)
+	CheckRange(min, value, max);
+	if value < min then
+		value = min;
+	end;
+	if value > max then
+		value = max;
+	end
+	return value;
+end;
+
+function Math.Modulo(min, value, max)
+	CheckRange(min, value, max);
+	value = value - min;
+	local range = max - min;
+	return min + value % range;
+end;
+
+function Math.Percent(min, value, max)
+	CheckRange(min, value, max);
+	value = value - min;
+	local range = max - min;
+	return value / range;
+end;
+
+function Math.Interpolate(min, value, max)
+	assert(type(min) == "number", "min must be a number");
+	assert(type(value) == "number", "value must be a number");
+	assert(type(max) == "number", "value must be a number");
+	local range = max - min;
+	return min + (range * value);
+end;
+
 function Math.Distance(...)
 	local x1, y1, x2, y2 = ...;
 	if select("#", ...) == 2 then
--- fritomod/tests/Math.lua
+++ fritomod/tests/Math.lua
@@ -4,3 +4,48 @@ function Suite:TestMean()
 	Assert.Equals(3, Math.Mean({3,3,3}));
 	Assert.Equals(3, Math.Mean(2,3,4));
 end;
+
+function Suite:TestClamp()
+	Assert.Equals(3, Math.Clamp(0, 3, 5));
+	Assert.Equals(5, Math.Clamp(0, 10, 5));
+	Assert.Equals(0, Math.Clamp(0, -3, 5));
+
+	Assert.Exception(Math.Clamp, nil, 3, 5);
+	Assert.Exception(Math.Clamp, 0, nil, 5);
+	Assert.Exception(Math.Clamp, 0, 3, nil);
+end;
+
+function Suite:TestModulo()
+	Assert.Equals(3, Math.Modulo(0, 3, 5));
+	Assert.Equals(0, Math.Modulo(0, 5, 5));
+	Assert.Equals(1, Math.Modulo(0, 6, 5));
+	Assert.Equals(4, Math.Modulo(0, -1, 5));
+
+	Assert.Exception(Math.Modulo, nil, 3, 5);
+	Assert.Exception(Math.Modulo, 0, nil, 5);
+	Assert.Exception(Math.Modulo, 0, 3, nil);
+end;
+
+function Suite:TestPercent()
+	Assert.Equals(.5, Math.Percent(0, 1, 2));
+	Assert.Equals(.5, Math.Percent(0, Math.Interpolate(0, .5, 2), 2));
+end;
+
+function Suite:TestInterpolate()
+	Assert.Equals(1, Math.Interpolate(0, .5, 2));
+	Assert.Equals(1, Math.Interpolate(2, .5, 0));
+end;
+
+function Suite:TestDistance()
+	-- I'm using a Pythagorean triple (3, 4, 5) here, for a readable
+	-- distance value.
+	Assert.Equals(5, Math.Distance(0, 0, 3, 4));
+	Assert.Equals(0, Math.Distance(1, 1, 1, 1));
+	Assert.Equals(5, Math.Distance({0, 0}, {3, 4}));
+end;
+
+function Suite:TestSignum()
+	Assert.Equals(1, Math.Signum(5));
+	Assert.Equals(-1, Math.Signum(-5));
+	Assert.Equals(0, Math.Signum(0));
+end;
