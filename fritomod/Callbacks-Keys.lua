469cc7936be214ade2d3c4e9bb9a4070cc1e5bcd
Aaron Faanes <dafrito@gmail.com>
2012-08-17 20:38:27 -0500

    Callbacks-Keys uses Frames.GetCallbackHandler

--- fritomod/Callbacks-Keys.lua
+++ fritomod/Callbacks-Keys.lua
@@ -1,45 +1,27 @@
 if nil ~= require then
 	require "fritomod/OOP-Class";
 	require "fritomod/ListenerList";
-	require "fritomod/ListenerList";
-end;
-
-local KeyListener = OOP.Class(ListenerList);
-
-local KEYBOARD_ENABLER = "__KeyboardEnabler";
-
-function KeyListener:Constructor(frame, event)
-	KeyListener.super.Constructor(self, "Key listener");
-	self.frame = frame;
-	self.event = event;
-
-	-- Enable the keyboard, if it's not already enabled.
-	self:AddInstaller(function()
-		if not frame[KEYBOARD_ENABLER] then
-			frame[KEYBOARD_ENABLER] = Functions.Install(
-				Seal(frame, "EnableKeyboard", true),
-				Noop);
-		end;
-		return frame[KEYBOARD_ENABLER]();
-	end);
-
-	self:AddInstaller(Callbacks.Script, self.frame, event, self, "Fire");
 end;
 
 Callbacks = Callbacks or {};
 
+local function ActivateKeyboard(frame)
+	local KEYBOARD_ENABLER = "__KeyboardEnabler";
+	if not frame[KEYBOARD_ENABLER] then
+		frame[KEYBOARD_ENABLER] = Functions.Install(
+			Seal(frame, "EnableKeyboard", true),
+			Noop);
+	end;
+	return frame[KEYBOARD_ENABLER]();
+end;
 
 local function KeyListener(event)
-	local LISTENER_NAME = "__"..event;
-
 	return function(frame, func, ...)
 		frame = Frames.AsRegion(frame);
 		assert(frame.EnableKeyboard, "Frame must support keyboard events");
-		func=Curry(func, ...);
-		if not frame[LISTENER_NAME] then
-			frame[LISTENER_NAME] = KeyListener:New(frame, event);
-		end;
-		return frame[LISTENER_NAME]:Add(func);
+		local list = Frames.GetCallbackHandler(frame, event,
+			ActivateKeyboard, frame);
+		return list:Add(func, ...);
 	end;
 end;
 
