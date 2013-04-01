b2d9ad04b5f8c0ae84b5c88849bc91dabc38d13c
Aaron Faanes <dafrito@gmail.com>
2013-03-31 21:44:30 -0500

    Implemented a text delegate

--- wow/FontString.lua
+++ wow/FontString.lua
@@ -7,34 +7,87 @@ end;
 local FontString=OOP.Class(WoW.Frame);
 WoW.FontString = FontString;
 
-function WoW.Frame:CreateFontString()
-	return WoW.FontString:New(self);
+WoW.RegisterFrameType("FontString", FontString, "New");
+WoW.RegisterFrameInheritance("FontString", "Frame");
+
+local Frame = WoW.Frame;
+
+function Frame:CreateFontString()
+	return FontString:New(self);
 end;
 
-function WoW.FontString:SetFontObject()
+function FontString:SetFontObject()
+    trace("STUB FontString:SetFontObject");
 end;
 
-function WoW.FontString:SetFont()
-end;
-
-function WoW.FontString:SetTextColor()
-end;
-
-function WoW.FontString:SetText()
-end;
-
-function WoW.FontString:GetStringWidth()
-	return 0
-end;
-
-function WoW.FontString:GetStringHeight()
-	return 0;
-end;
-
-function WoW.FontString:SetJustifyH()
+function FontString:SetJustifyH()
 
 end;
 
-function WoW.FontString:SetJustifyV()
+function FontString:SetJustifyV()
 
+end;
+
+WoW.Delegate(FontString, "text", {
+	"GetText",
+	"SetText",
+
+    "GetFont",
+    "SetFont",
+
+    "GetTextColor",
+    "SetTextColor",
+
+    "GetStringHeight",
+    "GetStringWidth",
+    "GetWrappedWidth"
+});
+
+local TestingTextDelegate = OOP.Class();
+
+if not WoW.GetFrameDelegate("FontString", "text") then
+    WoW.SetFrameDelegate("FontString", "text", TestingTextDelegate, "New");
+end;
+
+function TestingTextDelegate:Constructor(frame)
+    self.frame = frame;
+    self.text = "";
+    self.color = {1, 1, 1, 1};
+    self.font = {};
+end;
+
+function TestingTextDelegate:GetText()
+    return self.text;
+end;
+
+function TestingTextDelegate:SetText(text)
+    self.text = tostring(text);
+end;
+
+function TestingTextDelegate:GetFont()
+    return unpack(self.font);
+end;
+
+function TestingTextDelegate:SetFont(font, size, ...)
+    self.font = {font, size};
+end;
+
+function TestingTextDelegate:GetTextColor()
+    return unpack(self.color);
+end;
+
+function TestingTextDelegate:SetTextColor(r, g, b, a)
+    self.color = {r, g, b, a};
+end;
+
+function TestingTextDelegate:GetStringWidth()
+    return 0;
+end;
+
+function TestingTextDelegate:GetStringHeight()
+    return 0;
+end;
+
+function TestingTextDelegate:GetWrappedWidth()
+    return 0;
 end;
