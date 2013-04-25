e60d38f0cb46f0603c3705aefe13c0ce111de830
Aaron Faanes <dafrito@gmail.com>
2013-04-25 07:10:59 -0500

    Persistence reworking
    Most of these changes were made to prepare Persistence to be used
    outside of WoW. While I was here, I also cleaned up a bit of the code
    (e.g. adding aliases, improving variable names). The underlying behavior
    should be the same.

--- fritomod/Persistence.lua
+++ fritomod/Persistence.lua
@@ -3,56 +3,84 @@ if nil~=require then
 	require "fritomod/Functions";
 	require "fritomod/Lists";
 	require "fritomod/Events";
+	require "fritomod/Callbacks-Timing";
+end;
+
+Persistence = {};
+
+local loaded;
+local loaders={};
+local savers;
+
+local persisted;
+
+function Persistence.Loaded()
+    return loaded;
 end;
 
 local function AssertPersistence()
-		assert(_Persistence~=nil, "SavedVariables have not yet been loaded");
+    assert(Persistence.Loaded(), "Persisted values are not yet available");
 end;
 
-local loaded;
-Persistence=setmetatable({}, {
+function Persistence.RawData()
+    AssertPersistence();
+    return persisted;
+end;
+
+function Persistence.Load(sourceData)
+    assert(not Persistence.Loaded(), "Persisted values must not be reloaded");
+    assert(sourceData, "Source data must not be falsy");
+	loaded=true;
+    persisted=sourceData;
+    trace("Firing persistence loaders");
+	savers=Lists.MapCall(loaders, persisted);
+end;
+
+function Persistence.Save()
+    if savers then
+        trace("Firing persistence savers");
+        Lists.CallEach(savers);
+    end;
+end;
+
+setmetatable(Persistence, {
 	__index=function(self, key)
-		if key=="Loaded" then
-			return function()
-				return loaded;
-			end;
-		end;
 		AssertPersistence();
-		return _Persistence[key];
+		return persisted[key];
 	end,
 
 	__newindex=function(self, key, value)
 		-- TODO: This doesn't need to assert persistence; we could override these
 		-- values on load.
 		AssertPersistence();
-		_Persistence[key]=value;
+		persisted[key]=value;
 	end
 });
-
-local listeners={};
-local removers;
 
 Callbacks=Callbacks or {};
 function Callbacks.Persistence(func, ...)
 	func=Curry(func, ...);
 	if loaded then
 		Callbacks.Later(function()
-			local r=func();
-			if r then
-				table.insert(removers, r);
+			local saver=func();
+			if saver then
+				table.insert(savers, saver);
 			end;
 		end);
 	end;
-	return Lists.Insert(listeners, func);
+	return Lists.Insert(loaders, func);
 end;
+Callbacks.Persist = Callbacks.Persistence;
+Callbacks.Persistance = Callbacks.Persistence;
+Callbacks.Peristence = Callbacks.Persistence;
 
 function Callbacks.PersistentValue(key, func, ...)
 	func=Curry(func, ...);
 	return Callbacks.Persistence(function()
-		local remover=func(Persistence[key]);
-		if remover then
+		local saver=func(Persistence[key]);
+		if saver then
 			return function()
-				local newValue=remover(Persistence[key]);
+				local newValue=saver(Persistence[key]);
 				if newValue~=nil then
 					Persistence[key]=newValue;
 				end;
@@ -60,16 +88,16 @@ function Callbacks.PersistentValue(key, func, ...)
 		end;
 	end);
 end;
+Callbacks.PersistantValue = Callbacks.PersistentValue;
+Callbacks.PersistValue = Callbacks.PersistentValue;
+Callbacks.PeristValue = Callbacks.PersistentValue;
 
 Events.ADDON_LOADED(function(addon)
 	if addon:lower() ~= "fritomod" then
 		return;
 	end;
-	loaded=true;
-	_Persistence=_Persistence or {};
-	removers=Lists.MapCall(listeners);
+    _Persistence = _Persistence or {};
+    Persistence.Load(_Persistence);
 end);
 
-Events.PLAYER_LOGOUT(function(addon)
-	Lists.CallEach(removers);
-end);
+Events.PLAYER_LOGOUT(Persistence.Save);
