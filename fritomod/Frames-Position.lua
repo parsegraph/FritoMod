fd85631120e931f23c5c81b37cf15f63dd5b0f57
Aaron Faanes <dafrito@gmail.com>
2013-04-25 07:12:06 -0500

    Frames.Position improvements
    There's two changes here:
    
    First, Frames.Position supports an optional defaultPosition function
    that will be used if no saved position is available. I wasn't sure how
    to implement this previously, so it's nice to have a solution now.
    
    Second, all points are now saved, instead of just the first point. I
    tried to find why I only saved the first point, but I couldn't find any
    reason for it.

--- fritomod/Frames-Position.lua
+++ fritomod/Frames-Position.lua
@@ -10,18 +10,23 @@ end;
 local PERSISTENCE_KEY="FritoMod.PersistentFrames"
 
 local positionedFrames={};
+local defaultPositions = {};
 
-local function LoadPoint(name, frame, savedPosition)
+local function LoadPoint(name, frame, savedPosition, defaultPosition)
 	if not savedPosition then
 		trace("No saved position; using default for %q", name);
-		frame:SetPoint("center");
+        if defaultPosition then
+            defaultPosition(frame);
+        else
+            frame:SetPoint("center");
+        end;
 	end;
 	if type(savedPosition)=="table" and savedPosition.error then
 		print(("Save error found during loading %s: %s"):format(name, savedPosition.error));
 		savedPosition.error=nil;
 	end;
 	if frame:GetNumPoints()==0 then
-		local rv, err=pcall(Serializers.LoadPoint, savedPosition, frame);
+		local rv, err=pcall(Serializers.LoadAllPoints, savedPosition, frame);
 		if not rv then
 			-- This works for now, but it'd be nice if we could be more obvious
 			-- when this stuff fails.
@@ -30,18 +35,36 @@ local function LoadPoint(name, frame, savedPosition)
 	end;
 end;
 
-function Frames.Position(frame, name)
+function Frames.Position(frame, ...)
 	frame=Frames.AsRegion(frame);
-	if not name then
-		name=frame:GetName();
-	end;
+    local name, defaultPosition;
+
+    if select("#", ...) == 1 then
+        if IsCallable(...) then
+            defaultPosition = ...;
+        else
+            name = ...;
+        end;
+    elseif select("#", ...) > 1 then
+        local first = ...;
+        if IsCallable(first) then
+            defaultPosition = Curry(...);
+            name = nil;
+        else
+            name = first;
+            defaultPosition = Curry(select(2, ...));
+        end;
+    end;
+    name = name or frame:GetName();
 	positionedFrames[name]=frame;
 	if frame and Persistence.Loaded() then
 		local savedPosition;
 		if Persistence[PERSISTENCE_KEY] then
 			savedPosition=Persistence[PERSISTENCE_KEY][name];
 		end;
-		LoadPoint(name, frame, savedPosition);
+		LoadPoint(name, frame, savedPosition, defaultPosition);
+    elseif defaultPosition then
+        defaultPositions[name] = defaultPosition;
 	end;
 	return Functions.OnlyOnce(function()
 		positionedFrames[name]=nil;
@@ -54,11 +77,31 @@ function Frames.DumpPosition(name)
 	end;
 end;
 
+local function Upgrade(persistedFrames)
+    if persistedFrames.__version == nil then
+        local upgraded = {
+            __version = 2
+        };
+        for name, position in pairs(persistedFrames) do
+            if name ~= "__version" then
+                -- Convert single points to lists of points.
+                upgraded[name] = {position};
+            end;
+        end;
+        return upgraded;
+    else
+        return persistedFrames;
+    end;
+end;
+
 Callbacks.PersistentValue(PERSISTENCE_KEY, function(persistedFrames)
 	if persistedFrames then
+        persistedFrames = Upgrade(persistedFrames);
+        Persistence[PERSISTENCE_KEY] = persistedFrames;
+
 		-- Load the persisted value, if available, for any positioned frame.
 		for name,frame in pairs(positionedFrames) do
-			LoadPoint(name, frame, persistedFrames[name]);
+			LoadPoint(name, frame, persistedFrames[name], defaultPositions[name]);
 		end;
 	else
 		-- Set all positioned frames to defaults.
@@ -74,7 +117,7 @@ Callbacks.PersistentValue(PERSISTENCE_KEY, function(persistedFrames)
 		end;
 		persistedFrames=persistedFrames or {};
 		for name,frame in pairs(positionedFrames) do
-			local rv, savedPosition=pcall(Serializers.SavePoint, frame, 1);
+			local rv, savedPosition=pcall(Serializers.SaveAllPoints, frame, 1);
 			if rv then
 				persistedFrames[name]=savedPosition;
 			elseif persistedFrames[name] then
