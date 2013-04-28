7fa24f5a904ca076691d3d1061565fa112fd1626
Aaron Faanes <dafrito@gmail.com>
2013-04-28 09:21:59 -0500

    Fix a copy/paste bug

--- wow/Texture.lua
+++ wow/Texture.lua
@@ -29,7 +29,6 @@ local Delegate = OOP.Class();
 function Delegate:SetTexture(...)
     if select("#", ...) > 1 then
         self.color = {...};
-        Rainback.Update();
         return;
     end;
     trace("STUB SetTexture");
