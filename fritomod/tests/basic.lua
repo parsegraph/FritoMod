cf4ff76e2c46bef1694b7cefa5f0e7c00de68da5
Aaron Faanes <dafrito@gmail.com>
2013-06-15 22:59:24 -0500

    Allow UnpackAll's test to treat sensible unpacking as success

--- fritomod/tests/basic.lua
+++ fritomod/tests/basic.lua
@@ -7,6 +7,18 @@ function Suite:TestUnpackAll()
 	Assert.Equals(3, c, "C value");
 end;
 
+-- This is a bit of a controversial test. PUC-Rio Lua unpacks the 3, which causes
+-- an exception. However, LuaJIT only unpacks the first 1. Since UnpackAll explicitly
+-- warns about nil values causing issues, I decided to keep this test and ensure one of
+-- the two possibilities are handled. Specifically, I'm checking to make sure either the
+-- nil is detected, or the table is unpacked only up to the first nil.
 function Suite:TestUnpackAllRejectsNilValues()
-	Assert.Exception("UnpackAll rejects nil values", UnpackAll, {1,nil,3});
+    local success, result = pcall(UnpackAll, {1, nil, 3});
+    if not success then
+        -- Throwing an error is a valid response
+        return;
+    else
+        -- ... or just returning up to the first nil value
+        Assert.Equals({1}, result);
+    end;
 end;
