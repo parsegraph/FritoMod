cad1a5740a1730f7ae1726030d6e5eeab39fa721
Aaron Faanes <dafrito@gmail.com>
2013-06-16 23:41:59 -0500

    Make Callbacks.Later use a ListenerList
    This actually didn't remove listeners properly before.

--- fritomod/Callbacks-Timing.lua
+++ fritomod/Callbacks-Timing.lua
@@ -10,21 +10,19 @@ end;
 Callbacks=Callbacks or {};
 Timing = Timing or {};
 
-local callbacks = {};
+local callbacks = ListenerList:New();
 
 function Timing.Flush()
-    if #callbacks == 0 then
+    if not callbacks:HasListeners() then
         return;
     end;
     Log.Enter("Timing", "Calling deferred functions", "Calling", #callbacks, "deferred functions(s)");
 
-    local runningCallbacks = callbacks;
-    callbacks = {};
-    for i=1, #runningCallbacks do
-        runningCallbacks[i]();
-    end;
-    Log.Leave();
+    callbacks:Fire();
+    Log.Leave("Timing", nil, "Flush complete");
 end;
+
+callbacks:AddInstaller(Timing.OnUpdate, Timing.Flush);
 
 -- Call the specified callback "later." This allows for functions to be called after
 -- a OnUpdate event has fired, which may be necessary if changes to UI elements don't
@@ -32,12 +30,12 @@ end;
 --
 function Callbacks.Later(func, ...)
     Log.Enter("Timing", "Deferring functions", "Deferring function");
-    table.insert(callbacks, Curry(func, ...));
-	local remover;
-	remover=Timing.OnUpdate(function()
-        Timing.Flush();
-		remover();
-	end);
-    Log.Leave();
+    func = Curry(func, ...);
+    local remover;
+    remover = callbacks:Add(function()
+        func();
+        remover();
+    end);
+	Log.Leave();
 	return remover;
 end;
