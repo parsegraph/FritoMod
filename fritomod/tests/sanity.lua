5e39ac95a21b43ca84eabc85f4ce1cd0a5d1ae73
Aaron Faanes <dafrito@gmail.com>
2024-11-11 19:25:58 -0800

    Lua 5.4 apparently fixed some table removal inconsistencies

--- fritomod/tests/sanity.lua
+++ fritomod/tests/sanity.lua
@@ -116,14 +116,6 @@ function Suite:TestVarargsSizeIsConstant()
 	Assert.Equals(2, Count(nil, nil), "Varargs retains size in spite of nil values");
 end;
 
-function Suite:TestSparseArrayIsWipedOutDuringRemoval()
-	local list = {nil, nil, nil};
-	list[3] = true;
-	list[2] = true;
-	table.remove(list, 3);
-	Assert.Equals(0, #list, "Sparse array's size is lost due to table.remove");
-end;
-
 function Suite:TestUnpackWithNils()
 	local total, nonNils = Count(unpack({nil, 2}));
 	Assert.Equals(2, total, "Unpack unpacks correctly when array is of correct size");
@@ -155,34 +147,44 @@ function Suite:TestArrayLengthIsAbsurd()
 	assert(#inconsistencies > 0, "Array length is inconsistent");
 end;
 
--- Demonstrates that even primed arrays still have inconsistencies with array length.
--- While investigating supporting nil values in Curry, I found out that {nil, nil, nil, ...}
--- seemed to preserve its length in most cases as long as the last element was not nil.
--- This turned out to be false, as this test case demonstrates. Specifically, things
--- start falling apart around 17 insertions, and never get any better. It essentially
--- approaches the inconsistencies found in the previous test case.
---
--- It may be possible that supporting sub-16 nil arguments in currying is possible, but
--- it seems to be about as error-prone as one can imagine to code, probably not anywhere
--- near performant, will never handle trailing nil values properly, and is a very rare use-
--- case anyway.
-function Suite:TestAbsurdArrayLengthWithPrimedLengthTable()
-	local inconsistencies = {};
-	-- Changing the max intended length to 16 or lower will yield a failing test, meaning
-	-- this strategy seems to work. Of course, this is only testing one nil value.
-	for intendedLength=1, 100 do
-		for removalIndex=1, intendedLength-1 do
-			-- Creates a list that seems to be primed such that its length is more consistent
-			-- than one created by table.insert alone.
-			local code = ("return { %s }"):format(("true, "):rep(intendedLength));
-			local list = assert(loadstring(code))();
-			list[removalIndex] = nil;
-			if #list ~= intendedLength then
-				table.insert(inconsistencies, {insertions, removalIndex, #list});
-			end;
-		end;
-	end;
-	assert(#inconsistencies > 0, "Array length is still not consistent");
+if luaversion() < 50400 then
+    function Suite:TestSparseArrayIsWipedOutDuringRemoval()
+        local list = {nil, nil, nil};
+        list[3] = true;
+        list[2] = true;
+        table.remove(list, 3);
+        Assert.Equals(0, #list, "Sparse array's size is lost due to table.remove");
+    end;
+
+    -- Demonstrates that even primed arrays still have inconsistencies with array length.
+    -- While investigating supporting nil values in Curry, I found out that {nil, nil, nil, ...}
+    -- seemed to preserve its length in most cases as long as the last element was not nil.
+    -- This turned out to be false, as this test case demonstrates. Specifically, things
+    -- start falling apart around 17 insertions, and never get any better. It essentially
+    -- approaches the inconsistencies found in the previous test case.
+    --
+    -- It may be possible that supporting sub-16 nil arguments in currying is possible, but
+    -- it seems to be about as error-prone as one can imagine to code, probably not anywhere
+    -- near performant, will never handle trailing nil values properly, and is a very rare use-
+    -- case anyway.
+    function Suite:TestAbsurdArrayLengthWithPrimedLengthTable()
+        local inconsistencies = {};
+        -- Changing the max intended length to 16 or lower will yield a failing test, meaning
+        -- this strategy seems to work. Of course, this is only testing one nil value.
+        for intendedLength=1, 100 do
+            for removalIndex=1, intendedLength-1 do
+                -- Creates a list that seems to be primed such that its length is more consistent
+                -- than one created by table.insert alone.
+                local code = ("return { %s }"):format(("true, "):rep(intendedLength));
+                local list = assert(loadstring(code))();
+                list[removalIndex] = nil;
+                if #list ~= intendedLength then
+                    table.insert(inconsistencies, {insertions, removalIndex, #list});
+                end;
+            end;
+        end;
+        assert(#inconsistencies > 0, "Array length is still not consistent");
+    end;
 end;
 
 function Suite:TestMetatableIsCalledOnTableInsertion()
